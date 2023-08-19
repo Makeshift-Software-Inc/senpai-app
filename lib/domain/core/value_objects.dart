12cc12e042d028790187f52db18dbb5762ca6ce8
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-19 14:56:31 +0300

    WIP(auth): Use dartz functional style for error handling

---
+++ lib/domain/core/value_objects.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:dartz/dartz.dart';
+import 'package:senpai/domain/core/errors.dart';
+import 'package:senpai/domain/core/failures.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  bool isValid() => value.isRight();
+
+  T getOrCrash() {
+    return value.fold((f) => throw UnexpectedValueError(f), (r) => r);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ValueObject<T> && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
