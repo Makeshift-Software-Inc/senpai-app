aa4589761d8cae63f4f501ffc7e1122afe79d940
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-21 00:10:52 +0300

    refactor(http): remove unnecessary class to make DI easier

--- lib/core/graphql/graphql_client.dart
+++ lib/core/graphql/graphql_client.dart
@@ -1,16 +1,14 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:senpai/models/env_model.dart';
 
-class GraphQlConfig {
-  GraphQLClient initGraphQLClient(EnvModel env) {
-    final httpLink = HttpLink(env.graphApiUrl);
+GraphQLClient initGraphQLClient(EnvModel env) {
+  final httpLink = HttpLink(env.graphApiUrl);
 
-    return GraphQLClient(
-      link: httpLink,
-      cache: GraphQLCache(),
-      defaultPolicies: DefaultPolicies(
-        subscribe: Policies(fetch: FetchPolicy.noCache),
-      ),
-    );
-  }
+  return GraphQLClient(
+    link: httpLink,
+    cache: GraphQLCache(),
+    defaultPolicies: DefaultPolicies(
+      subscribe: Policies(fetch: FetchPolicy.noCache),
+    ),
+  );
 }
