861c4502e2bfd027ba6e00d5c0f0a5c250bf17d0
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-20 22:35:21 +0300

    feat(http): Initialise graphql client
    Add a few ql static models based on data from insomia

---
+++ lib/core/graphql/graphql_client.dart
@@ -0,0 +1,10 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:senpai/models/env_model.dart';
+
+class GraphQlConfig {
+  GraphQLClient initGraphQLClient(EnvModel env) {
+    final httpLink = HttpLink(env.apiUrl);
+
+    return GraphQLClient(link: httpLink, cache: GraphQLCache());
+  }
+}
