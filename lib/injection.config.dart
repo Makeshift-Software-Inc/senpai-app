86067491ee8d9792f6fc2ca8858fb1cbcbeb01f1
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-20 22:55:29 +0300

    Add secure storage for token
    Will securely store data for the logged in user on the device

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -9,8 +9,12 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i8;
+import 'package:fresh_dio/fresh_dio.dart' as _i5;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:senpai/core/secure_storage/secure_auth_storage.dart' as _i7;
+import 'package:senpai/models/auth/auth_model.dart' as _i6;
 import 'package:senpai/models/env_model.dart' as _i3;
 import 'package:senpai/models/theme_model.dart' as _i4;
 
@@ -33,6 +37,8 @@ extension GetItInjectableX on _i1.GetIt {
       () => _i4.ThemeModel.create(),
       preResolve: true,
     );
+    gh.lazySingleton<_i5.TokenStorage<_i6.AuthModel>>(
+        () => _i7.SecureAuthStorage(gh<_i8.FlutterSecureStorage>()));
     return this;
   }
 }
