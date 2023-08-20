80bf10407ff57762537939379b146729dcbe907b
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-21 00:15:14 +0300

    refactor(structure): Move di to same folder

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 // import 'package:flutter/services.dart';
 import 'package:senpai/core/app/app.dart';
-import 'package:senpai/injection.dart';
+import 'package:senpai/dependency_injection/injection.dart';
 import 'package:senpai/theme/app_theme.dart';
 // import 'package:universal_platform/universal_platform.dart';
 // import 'package:flutter_displaymode/flutter_displaymode.dart';
