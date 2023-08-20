2021d9c533feab838c4e77f730a59b81667c6e74
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-21 00:16:15 +0300

    refactor(structure): Update residue imports

--- lib/core/dio/interceptors/bad_network_error_interceptor.dart
+++ lib/core/dio/interceptors/bad_network_error_interceptor.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:senpai/core/dio/dio_exception_handler.dart';
-import 'package:senpai/injection.dart';
+import 'package:senpai/dependency_injection/injection.dart';
 
 class BadNetworkErrorInterceptor extends Interceptor {
   @override
