45bc7ac4a3e6ccb261d682287f9ae092cf78c066
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-20 22:43:09 +0300

    feat(http): Set no cache default policy for ql client

--- lib/core/graphql/graphql_client.dart
+++ lib/core/graphql/graphql_client.dart
@@ -5,6 +5,12 @@ class GraphQlConfig {
   GraphQLClient initGraphQLClient(EnvModel env) {
     final httpLink = HttpLink(env.apiUrl);
 
-    return GraphQLClient(link: httpLink, cache: GraphQLCache());
+    return GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(),
+      defaultPolicies: DefaultPolicies(
+        subscribe: Policies(fetch: FetchPolicy.noCache),
+      ),
+    );
   }
 }
