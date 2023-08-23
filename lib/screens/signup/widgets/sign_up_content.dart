ef6adcc4601a1bcecaa28e3ccf3347293acd84df
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-23 03:06:05 +0300

    refactor(auth): Go stateless on the signup content

--- lib/screens/signup/widgets/sign_up_content.dart
+++ lib/screens/signup/widgets/sign_up_content.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class SignUpContent extends StatefulWidget {
-  const SignUpContent({super.key});
+class SignupContent extends StatelessWidget {
+  const SignupContent({super.key});
 
-  @override
-  State<SignUpContent> createState() => _SignUpContentState();
-}
-
-class _SignUpContentState extends State<SignUpContent> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
