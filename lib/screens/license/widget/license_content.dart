3354a7c7e038f5ea219fc70c0a396c4a2ca14d1c
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-08-30 00:11:35 +0300

    feat(auth): Tweak styles on the markdown editor

--- lib/screens/license/widget/license_content.dart
+++ lib/screens/license/widget/license_content.dart
@@ -1,7 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:senpai/core/widgets/icon_button.dart';
 import 'package:senpai/core/widgets/loading.dart';
+import 'package:senpai/data/path_constants.dart';
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
 
 class LicenseContent extends StatelessWidget {
   const LicenseContent({super.key, required this.mdFilePath});
@@ -10,19 +15,52 @@ class LicenseContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: Future.delayed(const Duration(milliseconds: 150)).then((value) {
-          return rootBundle.loadString(mdFilePath);
-        }),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Markdown(
-              data: snapshot.data!,
-            );
-          }
-          return const Center(
-            child: SenpaiLoading(),
-          );
-        });
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+        child: Column(
+          children: [
+            SizedBox(
+              height: $constants.insets.xl,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SenpaiIconButton(
+                  onPressed: () {
+                    context.router.pop();
+                  },
+                  iconPath: PathConstants.backIcon,
+                )
+              ],
+            ),
+            SizedBox(
+              height: $constants.insets.lg,
+            ),
+            Expanded(
+              child: FutureBuilder(
+                  future: Future.delayed(const Duration(milliseconds: 150))
+                      .then((value) {
+                    return rootBundle.loadString(mdFilePath);
+                  }),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Markdown(
+                        data: snapshot.data!,
+                        styleSheet:
+                            MarkdownStyleSheet.fromTheme(Theme.of(context))
+                                .copyWith(
+                                    h1: getTextTheme(context).headlineLarge),
+                      );
+                    }
+                    return const Center(
+                      child: SenpaiLoading(),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
