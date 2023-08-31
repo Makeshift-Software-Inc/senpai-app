572b3eaac4d4436b06173b2368517ef0d1147a97
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-01 01:44:27 +0300

    WIP(navigation): Setup boilerplate pages for the main tab bar navigation.

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:senpai/routes/guards/onboarding_guard.dart';
+import 'package:senpai/screens/chat_list/page/chat_list_page.dart';
 import 'package:senpai/screens/entry/page/entry_page.dart';
 import 'package:senpai/screens/home/page/home_page.dart';
 import 'package:senpai/screens/license/page/license_page.dart';
+import 'package:senpai/screens/match/page/match_page.dart';
 import 'package:senpai/screens/onboarding/page/onboarding_page.dart';
 import 'package:senpai/screens/profile_fill/page/profile_fill.dart';
 import 'package:senpai/screens/signup/page/sign_up_page.dart';
@@ -20,12 +22,12 @@ class AppRouter extends _$AppRouter {
         AutoRoute(page: EntryRoute.page, path: '/entry'),
         AutoRoute(page: SignUpRoute.page, path: '/create_user'),
         AutoRoute(page: VerifyPhoneRoute.page, path: '/verify_phone'),
-        AutoRoute(page: ProfileFillRoute.page, path: '/profile_fill'),
         AutoRoute(page: SenpaiLicenseRoute.page, path: '/license'),
         AutoRoute(page: VerifyPhotoRoute.page, path: '/verify_photo'),
-        AutoRoute(
-          page: HomeRoute.page,
-          initial: true,
-        ),
+        AutoRoute(page: HomeRoute.page, initial: true, path: '/', children: [
+          AutoRoute(page: MatchRoute.page, path: '/match'),
+          AutoRoute(page: ChatListRoute.page, path: '/chat_list'),
+          AutoRoute(page: ProfileFillRoute.page, path: '/profile_fill'),
+        ]),
       ];
 }
---
+++ lib/screens/chat_list/page/chat_list_page.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class ChatListPage extends StatelessWidget {
+  const ChatListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("To be implemented"));
+  }
+}
