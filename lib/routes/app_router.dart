fb6eedfc0195f6a750804b90da74a75cdf721a66
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-01 00:31:19 +0300

    feat(tabs): Add main screen boilerplate.
    - this screen is home because it spins off main navigation to the central sections of the application where user will be spending most of the time.

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:senpai/routes/guards/onboarding_guard.dart';
 import 'package:senpai/screens/entry/page/entry_page.dart';
+import 'package:senpai/screens/home/page/home_page.dart';
 import 'package:senpai/screens/license/page/license_page.dart';
 import 'package:senpai/screens/onboarding/page/onboarding_page.dart';
 import 'package:senpai/screens/profile_fill/page/profile_fill.dart';
@@ -25,5 +26,8 @@ class AppRouter extends _$AppRouter {
         AutoRoute(page: ProfileFillRoute.page, path: '/profile_fill'),
         AutoRoute(page: SenpaiLicenseRoute.page, path: '/license'),
         AutoRoute(page: VerifyPhotoRoute.page, path: '/verify_photo'),
+        AutoRoute(
+          page: HomeRoute.page,
+        ),
       ];
 }
---
+++ lib/screens/home/page/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:senpai/screens/home/widgets/home_content.dart';
+
+@RoutePage()
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeContent(),
+    );
+  }
+}
