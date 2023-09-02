9d154c3c8341119e23db238dc6e9b9bdf2b6da97
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-03 02:44:56 +0300

    WIP(chat list): Add header for the chat list tab

--- lib/screens/chat_list/page/chat_list_page.dart
+++ lib/screens/chat_list/page/chat_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:senpai/screens/chat_list/widgets/chat_list_content.dart';
 
 @RoutePage()
 class ChatListPage extends StatelessWidget {
@@ -7,6 +8,6 @@ class ChatListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("To be implemented"));
+    return const SafeArea(child: ChatListContent());
   }
 }
