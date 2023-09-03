128614c130e5f53304543a778618de349a814c88
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-03 21:12:16 +0300

    refactor(chat list): Move sample data to mock file

--- lib/screens/chat_list/widgets/chat_list_content.dart
+++ lib/screens/chat_list/widgets/chat_list_content.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:senpai/core/widgets/icon_input.dart';
+import 'package:senpai/data/mock_match_data.dart';
 import 'package:senpai/data/path_constants.dart';
 import 'package:senpai/data/text_constants.dart';
-import 'package:senpai/models/match/match_user_data.dart';
 import 'package:senpai/screens/chat_list/widgets/match_list.dart';
 import 'package:senpai/utils/constants.dart';
 import 'package:senpai/utils/methods/utils.dart';
@@ -70,38 +70,7 @@ class ChatListContent extends StatelessWidget {
         ),
         Padding(
           padding: EdgeInsets.only(left: $constants.insets.lg),
-          child: MatchList(users: [
-            MatchUserData(
-              imageUrl:
-                  "https://images.pexels.com/photos/5226193/pexels-photo-5226193.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-              isOnline: true,
-              userName: "Mike",
-            ),
-            MatchUserData(
-              imageUrl:
-                  "https://images.pexels.com/photos/4926674/pexels-photo-4926674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-              isOnline: true,
-              userName: "Lewis",
-            ),
-            MatchUserData(
-              imageUrl:
-                  "https://images.pexels.com/photos/13876068/pexels-photo-13876068.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-              isOnline: false,
-              userName: "Fernando",
-            ),
-            MatchUserData(
-              imageUrl:
-                  "https://images.pexels.com/photos/6426224/pexels-photo-6426224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-              isOnline: false,
-              userName: "Henry",
-            ),
-            MatchUserData(
-              imageUrl:
-                  "https://images.pexels.com/photos/7256511/pexels-photo-7256511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-              isOnline: true,
-              userName: "Arthur",
-            ),
-          ]),
+          child: MatchList(users: mockMatchedUsers),
         ),
         SizedBox(
           height: $constants.insets.lg,
