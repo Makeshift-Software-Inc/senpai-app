8e316b0cd52f981cdc50076e81a4689abb62fb7f
vika_litslink <viktoriia.lekh@litslink.com>
2023-09-05 10:26:37 +0300

    feat(profile_fill): Add desired gender page

---
+++ lib/screens/profile_fill/desired_gender/widgets/desired_gender_content.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:senpai/core/widgets/primary_button.dart';
+import 'package:senpai/core/widgets/senpai_radio_button.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/screens/profile_fill/bloc/profile_fill_bloc.dart';
+import 'package:senpai/screens/profile_fill/desired_gender/bloc/desired_gender_bloc.dart';
+import 'package:senpai/screens/profile_fill/desired_gender/enums/desired_gender_enum.dart';
+import 'package:senpai/screens/profile_fill/widgets/header_simple_field.dart';
+import 'package:senpai/utils/constants.dart';
+
+class DesiredGenderContent extends StatelessWidget {
+  const DesiredGenderContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _buildHeader(context),
+          _buildRadioButtonList(context),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: _buildSubmitButton(context),
+            ),
+          ),
+          SizedBox(
+            height: $constants.insets.sm,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    final bloc = BlocProvider.of<ProfileFillBloc>(context);
+    return HeaderSimpleField(
+      title: TextConstants.youLookingForText,
+      description: TextConstants.youLookingDescription,
+      onTapBackButton: () {
+        bloc.add(
+          OnChangeStepEvent(
+            step: ProfileFillStep.gender,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildRadioButtonList(BuildContext context) {
+    final bloc = BlocProvider.of<DesiredGenderBloc>(context);
+    return BlocBuilder<DesiredGenderBloc, DesiredGenderState>(
+      builder: (context, state) {
+        return Column(
+          children: DesiredGender.values.map((gender) {
+            return Padding(
+              padding: EdgeInsets.only(right: $constants.corners.md),
+              child: SenpaiRadioButton(
+                title: genderToString(gender),
+                value: gender,
+                isSelected: gender == bloc.desiredGender,
+                onChanged: (desiredGender) {
+                  bloc.add(
+                    OnDesiredGenderChangedEvent(desiredGender: desiredGender),
+                  );
+                },
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildSubmitButton(BuildContext context) {
+    final bloc = BlocProvider.of<DesiredGenderBloc>(context);
+    final blocProfileFill = BlocProvider.of<ProfileFillBloc>(context);
+    return BlocListener<DesiredGenderBloc, DesiredGenderState>(
+      listenWhen: (_, currState) => currState is DesiredGenderSucssesfulState,
+      listener: (context, state) {
+        blocProfileFill.add(
+          OnDesiredGenderSaveEvent(
+            gender: genderToServer(bloc.desiredGender!),
+          ),
+        );
+      },
+      child: PrimaryButton(
+        text: TextConstants.nextText,
+        onPressed: () async {
+          bloc.add(NextTappedEvent());
+        },
+      ),
+    );
+  }
+}
