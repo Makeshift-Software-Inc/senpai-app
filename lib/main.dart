434a3269f76f0112cd1109f9ed5b2a3df8fdcd85
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-07 02:32:49 +0300

    feat(debug): Hook an observer to the BLOCs
    This commit also adds a storage space for hydrated BLOCs as I notice we are going to start using those with user ids and phones

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:injectable/injectable.dart';
-// import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:senpai/core/app/app.dart';
+import 'package:senpai/core/bloc_observer/bloc_observer.dart';
 import 'package:senpai/core/sentry/sentry_module.dart';
 import 'package:senpai/dependency_injection/injection.dart';
 import 'package:senpai/i18n/strings.g.dart';
 import 'package:senpai/theme/app_theme.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-// import 'package:universal_platform/universal_platform.dart';
-// import 'package:flutter_displaymode/flutter_displaymode.dart';
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 Future<void> main() async {
   await runZonedGuarded<Future<void>>(() async {
@@ -21,6 +21,15 @@ Future<void> main() async {
 
     // Inits sentry for error tracking.
     await initializeSentry();
+
+    // Set bloc observer and hydrated bloc storage.
+    Bloc.observer = Observer();
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: UniversalPlatform.isWeb
+          ? HydratedStorage.webStorageDirectory
+          : await getApplicationDocumentsDirectory(),
+    );
+
     final theme = await createTheme(brightness: Brightness.dark);
     runApp(
       DefaultAssetBundle(
