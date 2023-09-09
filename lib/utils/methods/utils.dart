41fba0a067d99056c9826dd0bcc62f425e7c0141
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-10 02:28:05 +0300

    fix(time utils): Fix time display util to correctly show AM and PM

--- lib/utils/methods/utils.dart
+++ lib/utils/methods/utils.dart
@@ -74,7 +74,7 @@ String formatDateTime(DateTime dateTime) {
   if (dateTime.year == now.year &&
       dateTime.month == now.month &&
       dateTime.day == now.day) {
-    return DateFormat('HH:mm').format(dateTime);
+    return DateFormat.jm().format(dateTime);
   } else if (dateTime.year == now.year &&
       dateTime.month == now.month &&
       dateTime.day == now.day - 1) {
