a791f673db893d1b4dfe69383d96d7391ae2226c
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-10 02:19:17 +0300

    feat(chat list): Add chat list widget content styles

--- lib/screens/chat_list/widgets/conversation_list.dart
+++ lib/screens/chat_list/widgets/conversation_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:senpai/core/widgets/user_avator.dart';
 import 'package:senpai/models/chat/chat_conversation.dart';
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
 
 class ConversationList extends StatelessWidget {
   final List<ChatConversation> chatList;
@@ -8,6 +11,114 @@ class ConversationList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: chatList.map(
+          (chat) {
+            return SizedBox(
+              width: double.infinity,
+              height: 80.0,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  UserAvatar(
+                    imageUrl: chat.profileUrl,
+                    size: 56,
+                  ),
+                  Expanded(
+                      child: Padding(
+                    padding: EdgeInsets.only(left: $constants.insets.sm),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: $constants.palette.lightBlue,
+                            width: 1.0,
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 56.0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    chat.contactName,
+                                    style: getTextTheme(context)
+                                        .headlineSmall!
+                                        .copyWith(
+                                          color: $constants.palette.white,
+                                        ),
+                                  ),
+                                  Text(
+                                    formatDateTime(chat.lastMessageTime),
+                                    style: getTextTheme(context)
+                                        .labelMedium!
+                                        .copyWith(
+                                          color: $constants.palette.grey,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  SizedBox(
+                                    width: getSize(context).width * 0.4,
+                                    child: Text(
+                                      chat.lastMessage,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: getTextTheme(context)
+                                          .bodyMedium!
+                                          .copyWith(
+                                            color: $constants.palette.white,
+                                          ),
+                                    ),
+                                  ),
+                                  if (chat.unreadMessagesCount > 0)
+                                    Container(
+                                      width: 25,
+                                      height: 25,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: $constants.palette.pink,
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          chat.unreadMessagesCount.toString(),
+                                          style: getTextTheme(context)
+                                              .labelMedium!
+                                              .copyWith(
+                                                color: $constants.palette.white,
+                                              ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ))
+                ],
+              ),
+            );
+          },
+        ).toList(),
+      ),
+    );
   }
 }
