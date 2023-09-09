582c0d946e389cbe76e96754be10783182cb1390
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-10 00:42:16 +0300

    feat(chat models): Add a conversation model
    This has some default values set for future use but will not be utilised in the current version of the application

---
+++ lib/models/chat/chat_conversation.dart
@@ -0,0 +1,25 @@
+class ChatConversation {
+  final String id;
+  final String contactName;
+  final String profileUrl;
+  final String lastMessage;
+  final DateTime lastMessageTime;
+  final int unreadMessagesCount;
+  final bool isTyping;
+  final bool isMuted;
+  final bool isPinned;
+  final bool hasAttachment;
+
+  ChatConversation({
+    required this.id,
+    required this.contactName,
+    required this.profileUrl,
+    required this.lastMessage,
+    required this.lastMessageTime,
+    required this.unreadMessagesCount,
+    this.isTyping = false,
+    this.isMuted = false,
+    this.isPinned = false,
+    this.hasAttachment = false,
+  });
+}
