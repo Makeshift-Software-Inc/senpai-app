b2dba11044a9f4144fb0220bc59329c1867ea331
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-10 01:23:39 +0300

    feat(chat list): Initialise conversation list widget with mock data

---
+++ lib/screens/chat_list/widgets/conversation_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:senpai/models/chat/chat_conversation.dart';
+
+class ConversationList extends StatelessWidget {
+  final List<ChatConversation> chatList;
+
+  const ConversationList({super.key, required this.chatList});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
