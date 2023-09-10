ff6276e30fca29d63a6f181f49dee56e04fccddf
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-09-10 15:38:09 +0300

    feat(chat): Add page for the chat screen

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:senpai/routes/guards/onboarding_guard.dart';
+import 'package:senpai/screens/chat/page/chat_page.dart';
 import 'package:senpai/screens/chat_list/page/chat_list_page.dart';
 import 'package:senpai/screens/entry/page/entry_page.dart';
 import 'package:senpai/screens/home/page/home_page.dart';
@@ -24,10 +25,15 @@ class AppRouter extends _$AppRouter {
         AutoRoute(page: VerifyPhoneRoute.page, path: '/verify_phone'),
         AutoRoute(page: SenpaiLicenseRoute.page, path: '/license'),
         AutoRoute(page: VerifyPhotoRoute.page, path: '/verify_photo'),
-        AutoRoute(page: HomeRoute.page, initial: true, path: '/', children: [
+        AutoRoute(page: HomeRoute.page, path: '/', children: [
           AutoRoute(page: MatchRoute.page, path: 'match'),
           AutoRoute(page: ChatListRoute.page, path: 'chat_list'),
           AutoRoute(page: ProfileFillRoute.page, path: 'profile_fill'),
         ]),
+        AutoRoute(
+          page: ChatRoute.page,
+          path: 'chat',
+          initial: true,
+        )
       ];
 }
