7bfcfba4d360ef9c9d3bf3bf539637721f85a88d
vika_litslink <viktoriia.lekh@litslink.com>
2023-09-14 18:55:12 +0300

    feat(university): add university bloc

---
+++ lib/screens/profile_fill/occupation/bloc/universities_bloc/universities_bloc.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/dependency_injection/injection.dart';
+import 'package:senpai/domain/profile_fill/universities_usecase.dart';
+import 'package:senpai/models/profile_fill/university/university_model.dart';
+
+part 'universities_event.dart';
+part 'universities_state.dart';
+
+class UniversitiesBloc extends Bloc<UniversitiesEvent, UniversitiesState> {
+  GetUniversitiesUseCase getUniversitiesUseCase =
+      getIt<GetUniversitiesUseCase>();
+
+  List<UniversityModel> universities = [];
+  String searchText = '';
+
+  UniversitiesBloc() : super(UniversitiesInitial()) {
+    on<OnUniversitiesFetchEvent>((event, emit) async {
+      if (event.searchText.isNotEmpty) {
+        emit(LoadingUniversitiesState());
+        try {
+          final result = await getUniversitiesUseCase.fetchUniversities(
+            event.searchText,
+          );
+          result.fold(
+            (failure) {
+              emit(ErrorUniversitiesState(
+                message: TextConstants.serverError,
+                isEnabled: true,
+              ));
+            },
+            (universityList) {
+              emit(UniversitiesSucssesfulState());
+              searchText = event.searchText;
+              universities = universityList;
+            },
+          );
+        } catch (_) {
+          emit(ErrorUniversitiesState(
+            message: TextConstants.serverError,
+            isEnabled: true,
+          ));
+        }
+      } else {
+        universities = [];
+        searchText = '';
+        emit(ErrorUniversitiesState(
+          message: TextConstants.serverError,
+          isEnabled: false,
+        ));
+      }
+    });
+  }
+}
