0fc200805de77a6693050c98916f320c48ee11ab
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-10-15 00:17:08 +0300

    fix(routes): Revert changes to pop all existing routes
    - This is probably temporary until we find a good fix for this, might end up restructuring the routes altogether.

--- lib/screens/verify_phone/page/verify_phone_page.dart
+++ lib/screens/verify_phone/page/verify_phone_page.dart
@@ -195,10 +195,8 @@ class VerifyPhonePage extends StatelessWidget {
                       bool hasFilledProfile = formBloc.isProfileFilled;
 
                       if (hasFilledProfile) {
-                        context.router.popUntilRoot();
                         context.router.pushNamed("/home");
                       } else {
-                        context.router.popUntilRoot();
                         context.router.pushNamed("/profile_fill");
                       }
 
