0d184b6529b34f7674231f645ceedd8798acabd5
vika_litslink <viktoriia.lekh@litslink.com>
2023-10-23 23:20:17 +0300

    upload photo gallery add image widget

---
+++ lib/screens/profile_fill/photos/upload_photos/gallery/widgets/custom_asset_image.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:senpai/utils/constants.dart';
+
+class CustomAssetImage extends StatelessWidget {
+  final AssetEntity image;
+  final double? width;
+  final double? height;
+
+  const CustomAssetImage(
+    this.image, {
+    super.key,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AssetEntityImage(
+      image,
+      isOriginal: false,
+      thumbnailSize: const ThumbnailSize.square(250),
+      fit: BoxFit.cover,
+      width: width,
+      height: height,
+      errorBuilder: (context, error, stackTrace) {
+        return Center(
+          child: Icon(
+            Icons.error,
+            color: $constants.palette.red,
+          ),
+        );
+      },
+    );
+  }
+}
