90f248c7ba353378614d02add1ebf82bab1f3329
vika_litslink <viktoriia.lekh@litslink.com>
2023-10-24 21:47:25 +0300

    upload photo add bloc for gallery

---
+++ lib/screens/profile_fill/photos/upload_photos/gallery/bloc/upload_photos_state.dart
@@ -0,0 +1,20 @@
+part of 'upload_photos_bloc.dart';
+
+@immutable
+abstract class UploadPhotosState {}
+
+class GalleryInitial extends UploadPhotosState {}
+
+class ErrorState extends UploadPhotosState {
+  final String message;
+
+  final bool isEnabled;
+
+  ErrorState({required this.message, required this.isEnabled});
+}
+
+class ValidState extends UploadPhotosState {}
+
+class LoadingUploadPhotosState extends UploadPhotosState {}
+
+class ChangeStepSucssesfulState extends UploadPhotosState {}
