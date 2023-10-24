90893a191ae655c1b3a2f2a1af53325ab7e421df
vika_litslink <viktoriia.lekh@litslink.com>
2023-10-24 22:03:49 +0300

    upload photos add upload photos manager page

---
+++ lib/screens/profile_fill/photos/upload_photos/page/upload_photos_manager_page.dart
@@ -0,0 +1,51 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/core/widgets/loading.dart';
+import 'package:senpai/screens/profile_fill/photos/bloc/photos_bloc.dart';
+import 'package:senpai/screens/profile_fill/photos/upload_photos/gallery/bloc/upload_photos_bloc.dart';
+
+import 'package:senpai/screens/profile_fill/photos/upload_photos/widgets/upload_photos_manager_content.dart';
+
+import 'package:senpai/utils/constants.dart';
+
+@RoutePage()
+class UploadPhotosManagerPage extends StatelessWidget {
+  final PhotosBloc bloc;
+
+  const UploadPhotosManagerPage({super.key, required this.bloc});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: bloc),
+        BlocProvider<UploadPhotosBloc>(
+          create: (context) => UploadPhotosBloc()
+            ..add(OnInitAssetsListLenghtEvent(
+              count: bloc.uploadedPhotos.length,
+            )),
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: $constants.palette.darkBlue,
+        body: Stack(
+          children: [
+            const UploadPhotosManagerContent(),
+            _buildGalleryLoadingListeners()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGalleryLoadingListeners() {
+    return BlocBuilder<UploadPhotosBloc, UploadPhotosState>(
+      builder: (context, state) {
+        return state is LoadingUploadPhotosState
+            ? const SenpaiLoading()
+            : const SizedBox.shrink();
+      },
+    );
+  }
+}
