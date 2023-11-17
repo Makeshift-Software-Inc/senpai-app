ec6e5e37d9ef4051a8dd7cc2368f17067fb83183
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-11-17 11:04:49 +0300

    feat(setup): Generate updated flutter platform plugins

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_localization
   flutter_secure_storage_windows
+  geolocator_windows
   permission_handler_windows
   sentry_flutter
   url_launcher_windows
