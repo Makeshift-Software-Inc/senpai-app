99afac99aa0250cd801c930d6dfb4cf049854aa4
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-21 17:43:13 +0200

    verify photo add start match content

---
+++ lib/screens/verify_photo/widgets/start_match_content.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/core/user/blocs/update_user/update_user_bloc.dart';
+import 'package:senpai/core/widgets/primary_button.dart';
+import 'package:senpai/data/path_constants.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/screens/profile_fill/bloc/profile_fill_bloc.dart';
+import 'package:senpai/screens/verify_photo/bloc/verify_photo_bloc.dart';
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
+
+class StartMatchContent extends StatelessWidget {
+  const StartMatchContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: _buildHeader(context),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                PathConstants.startMatchBg,
+                height: getSize(context).height * 0.3,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: $constants.insets.xl,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: _buildStartMatchButton(context),
+          ),
+          SizedBox(
+            height: $constants.insets.sm,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    final bloc = BlocProvider.of<VerifyPhotoBloc>(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          TextConstants.thanksLotText,
+          style: getTextTheme(context)
+              .headlineLarge
+              ?.copyWith(color: $constants.palette.white),
+        ),
+        if (bloc.photo != null)
+          Text(
+            TextConstants.weReceivedYourPhotoText,
+            style: getTextTheme(context).bodySmall?.copyWith(
+                  color: $constants.palette.grey,
+                  letterSpacing: 0,
+                ),
+            textAlign: TextAlign.center,
+          ),
+      ],
+    );
+  }
+
+  Widget _buildStartMatchButton(BuildContext context) {
+    final bloc = BlocProvider.of<VerifyPhotoBloc>(context);
+    final blocProfileFill = BlocProvider.of<ProfileFillBloc>(context);
+    final serverBloc = BlocProvider.of<UpdateUserBloc>(context);
+    return BlocListener<VerifyPhotoBloc, VerifyPhotoState>(
+      listenWhen: (_, currState) => currState is VerifyPhotoSucssesfulState,
+      listener: (context, state) {
+        serverBloc.updateUserInfo(user: blocProfileFill.user);
+      },
+      child: PrimaryButton(
+        text: TextConstants.startMatchingButton,
+        onPressed: () async {
+          bloc.add(OnStartMatchingEvent());
+        },
+      ),
+    );
+  }
+}
