f4c92f7d42e359d83b11f8da61512a21f741b4d0
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-21 15:52:36 +0200

    photo profile fill add photo content

---
+++ lib/screens/profile_fill/photos/widgets/photos_content.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:senpai/core/widgets/primary_button.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/screens/profile_fill/bloc/profile_fill_bloc.dart';
+import 'package:senpai/screens/profile_fill/photos/bloc/photos_bloc.dart';
+import 'package:senpai/screens/profile_fill/photos/widgets/list_photos_widget.dart';
+import 'package:senpai/screens/profile_fill/widgets/header_simple_field.dart';
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
+
+class PhotosContent extends StatelessWidget {
+  const PhotosContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _buildHeader(context),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ListPhotosWidget(),
+                  SizedBox(
+                    height: $constants.insets.sm,
+                  ),
+                  _buildHelperText(
+                    context,
+                    TextConstants.youCanChangeOrder,
+                  ),
+                  _buildHelperText(
+                    context,
+                    TextConstants.justDragPhoto,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: $constants.insets.sm,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: _buildSubmitButton(context),
+          ),
+          SizedBox(
+            height: $constants.insets.sm,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    final bloc = BlocProvider.of<ProfileFillBloc>(context);
+    return HeaderSimpleField(
+      title: TextConstants.uploadPhotosText,
+      description: TextConstants.uploadPhotosDescription,
+      onTapBackButton: () {
+        bloc.add(
+          OnChangeStepEvent(
+            step: ProfileFillStep.biography,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildHelperText(BuildContext context, String text) {
+    return Text(
+      text,
+      style: getTextTheme(context)
+          .labelMedium
+          ?.copyWith(color: $constants.palette.grey),
+    );
+  }
+
+  Widget _buildSubmitButton(BuildContext context) {
+    final bloc = BlocProvider.of<PhotosBloc>(context);
+    final blocProfileFill = BlocProvider.of<ProfileFillBloc>(context);
+
+    return BlocListener<PhotosBloc, PhotosState>(
+      listenWhen: (_, currState) => currState is PhotosSucssesfulState,
+      listener: (context, state) {
+        blocProfileFill.add(OnPhotosListSaveEvent(photos: bloc.uploadedPhotos));
+      },
+      child: PrimaryButton(
+        text: TextConstants.nextText,
+        onPressed: () async {
+          bloc.add(NextTappedEvent());
+        },
+      ),
+    );
+  }
+}
