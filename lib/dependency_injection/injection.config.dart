60b1137501a39ce6fbaf6266166f3709d3a0d03f
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-21 15:44:54 +0200

    add delete  photo  bloc

--- lib/dependency_injection/injection.config.dart
+++ lib/dependency_injection/injection.config.dart
@@ -9,40 +9,42 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i23;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i8;
-import 'package:fresh_dio/fresh_dio.dart' as _i17;
+import 'package:dio/dio.dart' as _i24;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i9;
+import 'package:fresh_dio/fresh_dio.dart' as _i18;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:graphql_flutter/graphql_flutter.dart' as _i10;
+import 'package:graphql_flutter/graphql_flutter.dart' as _i11;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart'
-    as _i11;
+    as _i12;
 import 'package:senpai/core/auth/blocs/create_user_bloc.dart' as _i4;
 import 'package:senpai/core/auth/blocs/resend_verification_code_bloc.dart'
-    as _i14;
-import 'package:senpai/core/auth/blocs/sign_in_bloc.dart' as _i15;
-import 'package:senpai/core/auth/blocs/validate_phone_bloc.dart' as _i22;
-import 'package:senpai/core/profile_fill/api/universities_api.dart' as _i20;
+    as _i15;
+import 'package:senpai/core/auth/blocs/sign_in_bloc.dart' as _i16;
+import 'package:senpai/core/auth/blocs/validate_phone_bloc.dart' as _i23;
+import 'package:senpai/core/profile_fill/api/universities_api.dart' as _i21;
+import 'package:senpai/core/profile_fill/blocs/upload_photo/delete_photo/delete_photo_bloc.dart'
+    as _i5;
 import 'package:senpai/core/profile_fill/blocs/upload_photo/reorder_photos/reorder_photos_bloc.dart'
-    as _i13;
+    as _i14;
 import 'package:senpai/core/profile_fill/blocs/upload_photo/upload_photo/upload_photo_bloc.dart'
-    as _i21;
+    as _i22;
 import 'package:senpai/core/profile_fill/favorite_anime/fetch_anime_bloc.dart'
-    as _i6;
-import 'package:senpai/core/secure_storage/secure_auth_storage.dart' as _i19;
-import 'package:senpai/core/user/blocs/fetch_user/fetch_user_bloc.dart' as _i7;
-import 'package:senpai/dependency_injection/dio_client_di.dart' as _i29;
-import 'package:senpai/dependency_injection/graphql_client_di.dart' as _i27;
-import 'package:senpai/dependency_injection/network_info_di.dart' as _i28;
-import 'package:senpai/dependency_injection/router_di.dart' as _i24;
-import 'package:senpai/dependency_injection/secure_storage_di.dart' as _i25;
-import 'package:senpai/dependency_injection/university_module_di.dart' as _i26;
-import 'package:senpai/domain/profile_fill/universities_usecase.dart' as _i9;
-import 'package:senpai/models/auth/auth_model.dart' as _i18;
-import 'package:senpai/models/env_model.dart' as _i5;
-import 'package:senpai/models/theme_model.dart' as _i16;
+    as _i7;
+import 'package:senpai/core/secure_storage/secure_auth_storage.dart' as _i20;
+import 'package:senpai/core/user/blocs/fetch_user/fetch_user_bloc.dart' as _i8;
+import 'package:senpai/dependency_injection/dio_client_di.dart' as _i30;
+import 'package:senpai/dependency_injection/graphql_client_di.dart' as _i28;
+import 'package:senpai/dependency_injection/network_info_di.dart' as _i29;
+import 'package:senpai/dependency_injection/router_di.dart' as _i25;
+import 'package:senpai/dependency_injection/secure_storage_di.dart' as _i26;
+import 'package:senpai/dependency_injection/university_module_di.dart' as _i27;
+import 'package:senpai/domain/profile_fill/universities_usecase.dart' as _i10;
+import 'package:senpai/models/auth/auth_model.dart' as _i19;
+import 'package:senpai/models/env_model.dart' as _i6;
+import 'package:senpai/models/theme_model.dart' as _i17;
 import 'package:senpai/routes/app_router.dart' as _i3;
-import 'package:senpai/utils/helpers/logging_helpers.dart' as _i12;
+import 'package:senpai/utils/helpers/logging_helpers.dart' as _i13;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -63,47 +65,48 @@ extension GetItInjectableX on _i1.GetIt {
     final dioInjection = _$DioInjection();
     gh.factory<_i3.AppRouter>(() => routerInjection.router());
     gh.factory<_i4.CreateUserBloc>(() => _i4.CreateUserBloc());
-    await gh.singletonAsync<_i5.EnvModel>(
-      () => _i5.EnvModel.create(),
+    gh.factory<_i5.DeletePhotoBloc>(() => _i5.DeletePhotoBloc());
+    await gh.singletonAsync<_i6.EnvModel>(
+      () => _i6.EnvModel.create(),
       preResolve: true,
     );
-    gh.factory<_i6.FetchAnimeBloc>(() => _i6.FetchAnimeBloc());
-    gh.factory<_i7.FetchUserBloc>(() => _i7.FetchUserBloc());
-    gh.factory<_i8.FlutterSecureStorage>(
+    gh.factory<_i7.FetchAnimeBloc>(() => _i7.FetchAnimeBloc());
+    gh.factory<_i8.FetchUserBloc>(() => _i8.FetchUserBloc());
+    gh.factory<_i9.FlutterSecureStorage>(
         () => secureStorageInjection.storage());
-    gh.factory<_i9.GetUniversitiesUseCase>(
+    gh.factory<_i10.GetUniversitiesUseCase>(
         () => universityInjectionModule.universitiesRepository);
-    gh.factory<_i10.GraphQLClient>(
-        () => graphQLInjection.graphql(gh<_i5.EnvModel>()));
-    gh.factory<_i11.InternetConnection>(() => networkInfoInjection.networkInfo);
-    gh.singleton<_i12.LoggingHelper>(_i12.LoggingHelper());
-    gh.factory<_i13.ReorderPhotosBloc>(() => _i13.ReorderPhotosBloc());
-    gh.factory<_i14.ResendVerificationCodeBloc>(
-        () => _i14.ResendVerificationCodeBloc());
-    gh.factory<_i15.SignInBloc>(() => _i15.SignInBloc());
-    await gh.singletonAsync<_i16.ThemeModel>(
-      () => _i16.ThemeModel.create(),
+    gh.factory<_i11.GraphQLClient>(
+        () => graphQLInjection.graphql(gh<_i6.EnvModel>()));
+    gh.factory<_i12.InternetConnection>(() => networkInfoInjection.networkInfo);
+    gh.singleton<_i13.LoggingHelper>(_i13.LoggingHelper());
+    gh.factory<_i14.ReorderPhotosBloc>(() => _i14.ReorderPhotosBloc());
+    gh.factory<_i15.ResendVerificationCodeBloc>(
+        () => _i15.ResendVerificationCodeBloc());
+    gh.factory<_i16.SignInBloc>(() => _i16.SignInBloc());
+    await gh.singletonAsync<_i17.ThemeModel>(
+      () => _i17.ThemeModel.create(),
       preResolve: true,
     );
-    gh.lazySingleton<_i17.TokenStorage<_i18.AuthModel>>(
-        () => _i19.SecureAuthStorage(gh<_i8.FlutterSecureStorage>()));
-    gh.factory<_i20.UniversitiesApi>(
-        () => universityInjectionModule.universitiesApi(gh<_i5.EnvModel>()));
-    gh.factory<_i21.UploadPhotoBloc>(() => _i21.UploadPhotoBloc());
-    gh.factory<_i22.ValidatePhoneBloc>(() => _i22.ValidatePhoneBloc());
-    gh.factory<_i23.Dio>(() => dioInjection.dio(gh<_i5.EnvModel>()));
+    gh.lazySingleton<_i18.TokenStorage<_i19.AuthModel>>(
+        () => _i20.SecureAuthStorage(gh<_i9.FlutterSecureStorage>()));
+    gh.factory<_i21.UniversitiesApi>(
+        () => universityInjectionModule.universitiesApi(gh<_i6.EnvModel>()));
+    gh.factory<_i22.UploadPhotoBloc>(() => _i22.UploadPhotoBloc());
+    gh.factory<_i23.ValidatePhoneBloc>(() => _i23.ValidatePhoneBloc());
+    gh.factory<_i24.Dio>(() => dioInjection.dio(gh<_i6.EnvModel>()));
     return this;
   }
 }
 
-class _$RouterInjection extends _i24.RouterInjection {}
+class _$RouterInjection extends _i25.RouterInjection {}
 
-class _$SecureStorageInjection extends _i25.SecureStorageInjection {}
+class _$SecureStorageInjection extends _i26.SecureStorageInjection {}
 
-class _$UniversityInjectionModule extends _i26.UniversityInjectionModule {}
+class _$UniversityInjectionModule extends _i27.UniversityInjectionModule {}
 
-class _$GraphQLInjection extends _i27.GraphQLInjection {}
+class _$GraphQLInjection extends _i28.GraphQLInjection {}
 
-class _$NetworkInfoInjection extends _i28.NetworkInfoInjection {}
+class _$NetworkInfoInjection extends _i29.NetworkInfoInjection {}
 
-class _$DioInjection extends _i29.DioInjection {}
+class _$DioInjection extends _i30.DioInjection {}
