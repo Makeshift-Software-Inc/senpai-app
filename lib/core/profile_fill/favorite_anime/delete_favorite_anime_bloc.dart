8ec22c1229e7b5bdc299e2d4fbb1b62b5c849183
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-21 19:31:49 +0200

    add delete favorite anme bloc

---
+++ lib/core/profile_fill/favorite_anime/delete_favorite_anime_bloc.dart
@@ -0,0 +1,37 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:injectable/injectable.dart';
+import 'package:senpai/core/graphql/blocs/mutation/mutation_bloc.dart';
+import 'package:senpai/core/graphql/models/graphql_api.graphql.dart';
+
+@injectable
+class DeleteFavoriteAnimeBloc
+    extends MutationBloc<DeleteFavoriteAnime$Mutation> {
+  DeleteFavoriteAnimeBloc()
+      : super(options: _deleteFavoriteAnimeMutationOptions());
+
+  static WatchQueryOptions<Object?> _deleteFavoriteAnimeMutationOptions() {
+    return WatchQueryOptions(
+      document: DELETE_FAVORITE_ANIME_MUTATION_DOCUMENT,
+      fetchPolicy: FetchPolicy.noCache,
+    );
+  }
+
+  @override
+  DeleteFavoriteAnime$Mutation parseData(Map<String, dynamic>? data) {
+    return DeleteFavoriteAnime$Mutation.fromJson(data ?? <String, dynamic>{});
+  }
+
+  deleteFavoriteAnimeList({
+    required int userId,
+    required String animeId,
+  }) {
+    final variables = DeleteFavoriteAnimeArguments(
+      input: DeleteFavoriteAnimeInput(
+        userId: '$userId',
+        animeId: animeId,
+      ),
+    ).toJson();
+
+    run(variables);
+  }
+}
