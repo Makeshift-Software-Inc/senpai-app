676dbb582f57bedd97294adce9838c9360141163
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-21 17:02:32 +0200

    add favorite anime content

---
+++ lib/screens/profile_fill/favorite_anime/widgets/start_favorite_anime_content.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/core/profile_fill/favorite_anime/add_favorite_anime_bloc.dart';
+import 'package:senpai/core/profile_fill/favorite_anime/fetch_anime_bloc.dart';
+import 'package:senpai/core/widgets/icon_input.dart';
+
+import 'package:senpai/core/widgets/primary_button.dart';
+import 'package:senpai/data/path_constants.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/screens/profile_fill/bloc/profile_fill_bloc.dart';
+import 'package:senpai/screens/profile_fill/favorite_anime/bloc/favorite_anime_bloc.dart';
+import 'package:senpai/screens/profile_fill/favorite_anime/widgets/anime_step_header_widget.dart';
+import 'package:senpai/screens/profile_fill/favorite_anime/widgets/selected_anime_list.dart';
+import 'package:senpai/utils/constants.dart';
+
+class StartFavoriteAnimeContent extends StatelessWidget {
+  const StartFavoriteAnimeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const AnimeStepHeaderWidget(),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+            child: Column(
+              children: [
+                SizedBox(height: $constants.insets.md),
+                _buildTextInput(context),
+                _buildAnimeList(context),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: $constants.insets.sm),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+            child: _buildSubmitButton(context),
+          ),
+        ),
+        SizedBox(height: $constants.insets.sm),
+      ],
+    );
+  }
+
+  Widget _buildTextInput(BuildContext context) {
+    final bloc = BlocProvider.of<FavoriteAnimeBloc>(context);
+    final blocFetchAnime = BlocProvider.of<FetchAnimeBloc>(context);
+
+    return SenpaiIconInput(
+      onTapInput: () {
+        bloc.add(OnChangeAnimeStepEvent(step: FavoriteAnimeStep.searchContent));
+        blocFetchAnime.fetchAnime(page: bloc.page);
+      },
+      hintText: TextConstants.searchText,
+      controller: bloc.searchController,
+      onChange: (String search) {},
+      iconPath: PathConstants.searchIcon,
+    );
+  }
+
+  Widget _buildSubmitButton(BuildContext context) {
+    final bloc = BlocProvider.of<FavoriteAnimeBloc>(context);
+    final blocProfileFill = BlocProvider.of<ProfileFillBloc>(context);
+    final addAnimeBloc = BlocProvider.of<AddFavoriteAnimeBloc>(context);
+
+    return BlocConsumer<FavoriteAnimeBloc, FavoriteAnimeState>(
+      listenWhen: (_, currState) => currState is FavoriteAnimeSucssesfulState,
+      listener: (context, state) {
+        blocProfileFill.add(
+          OnFavoriteAnimeSaveEvent(animeList: bloc.selectedAnimeList),
+        );
+      },
+      builder: (context, state) {
+        return PrimaryButton(
+          text: '${TextConstants.doneText} '
+              '(${bloc.selectedAnimeList.length}/${bloc.maxAnimeCount})',
+          onPressed: () async {
+            addAnimeBloc.addFavoriteAnimeList(
+              userId: blocProfileFill.userId,
+              animeIds: bloc.selectedAnimeList.map((e) => e.id).toList(),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildAnimeList(BuildContext context) {
+    return const Expanded(
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: SelectedAnimeList(),
+      ),
+    );
+  }
+}
