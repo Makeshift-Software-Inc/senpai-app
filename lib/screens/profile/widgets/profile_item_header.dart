a78946286b67fc66af18df451969b489b2268e49
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-24 18:20:41 +0200

    profile add item header widget

---
+++ lib/screens/profile/widgets/profile_item_header.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:senpai/data/text_constants.dart';
+
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
+
+class ProfileItemHeader extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final bool isEmptyContent;
+  final String? titleButton;
+
+  const ProfileItemHeader({
+    super.key,
+    required this.title,
+    required this.onTap,
+    this.isEmptyContent = false,
+    this.titleButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: getTextTheme(context)
+                .bodyMedium
+                ?.copyWith(color: $constants.palette.white),
+          ),
+          Row(
+            children: [
+              if (isEmptyContent)
+                Text(
+                  titleButton ?? TextConstants.addTitle,
+                  style: getTextTheme(context)
+                      .bodySmall
+                      ?.copyWith(letterSpacing: 0),
+                ),
+              Icon(
+                Icons.chevron_right,
+                color: $constants.palette.white,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
