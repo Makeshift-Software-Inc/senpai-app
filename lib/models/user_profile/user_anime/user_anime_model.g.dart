f8e2e64c6de15ccba4a7a96ad9d1f9ff75b24c51
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-24 13:51:34 +0200

    profile add user anime model

---
+++ lib/models/user_profile/user_anime/user_anime_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_anime_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserAnimeModel _$$_UserAnimeModelFromJson(Map<String, dynamic> json) =>
+    _$_UserAnimeModel(
+      id: json['id'] as String,
+      title: json['title'] as String?,
+      cover: json['cover'] as String?,
+    );
+
+Map<String, dynamic> _$$_UserAnimeModelToJson(_$_UserAnimeModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'cover': instance.cover,
+    };
