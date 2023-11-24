bf756cb4b18cd2c001b5f6eca5bd522f2bef6f78
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-24 18:08:49 +0200

    profile add list tile button

---
+++ lib/screens/profile/widgets/profile_list_tile_button.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
+
+class ProfileListTileButton extends StatelessWidget {
+  final String title;
+  final String icon;
+  final VoidCallback onTap;
+  final bool hasBorder;
+
+  const ProfileListTileButton({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onTap,
+    this.hasBorder = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: $constants.insets.xxl,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular($constants.corners.lg),
+        shape: BoxShape.rectangle,
+        color: $constants.palette.lightBlue,
+        border: Border.all(
+          color:
+              hasBorder ? $constants.palette.red : $constants.palette.lightBlue,
+          width: 1.0,
+        ),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: $constants.insets.xs,
+        ),
+        onTap: onTap,
+        leading: _buildLeading(),
+        title: Text(
+          title,
+          style: getTextTheme(context).bodyMedium,
+        ),
+        trailing: Icon(
+          hasBorder ? Icons.error : Icons.chevron_right,
+          color: $constants.palette.white,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLeading() {
+    return Container(
+      padding: EdgeInsets.all($constants.corners.md),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular($constants.insets.xs),
+        shape: BoxShape.rectangle,
+        color: $constants.palette.buttonBorder,
+      ),
+      child: SvgPicture.asset(
+        icon,
+        width: $constants.insets.sm,
+        height: $constants.insets.sm,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
