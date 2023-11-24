00713443b210b09fd9dbd23059afc1471fd2e69c
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-24 13:58:40 +0200

    profile update verify photo bloc

--- lib/screens/verify_photo/bloc/verify_photo_event.dart
+++ lib/screens/verify_photo/bloc/verify_photo_event.dart
@@ -5,8 +5,14 @@ abstract class VerifyPhotoEvent {}
 
 class OnVerifyPhotoInitEvent extends VerifyPhotoEvent {
   final File? photo;
+  final bool? hasProfileFillBloc;
+  final int? userId;
 
-  OnVerifyPhotoInitEvent({this.photo});
+  OnVerifyPhotoInitEvent({
+    this.userId,
+    this.photo,
+    this.hasProfileFillBloc,
+  });
 }
 
 class OnOpenStartMatchSceenEvent extends VerifyPhotoEvent {}
