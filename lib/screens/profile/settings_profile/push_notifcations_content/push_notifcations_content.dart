209683f1ea65faf7b45614c9f43eabea0db78f02
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-27 18:46:27 +0200

    profile settings add push notification content UI

---
+++ lib/screens/profile/settings_profile/push_notifcations_content/push_notifcations_content.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/core/widgets/senpai_cupertino_switch.dart';
+import 'package:senpai/screens/profile/settings_profile/bloc/settings_profile_bloc.dart';
+import 'package:senpai/screens/profile/settings_profile/enums/settings_profile_enums.dart';
+
+import 'package:senpai/screens/profile/widgets/profile_app_bar.dart';
+import 'package:senpai/utils/constants.dart';
+
+class PushNotifcationsContent extends StatelessWidget {
+  const PushNotifcationsContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ProfileAppBar(
+          title: TextConstants.pushNotificationsTitle,
+          hasLeading: true,
+          onDoneTap: () {
+            final bloc = BlocProvider.of<SettingsProfileBloc>(context);
+            print(bloc.selectedPushNotificationOptions.length);
+          },
+          onTapLeading: () {
+            final bloc = BlocProvider.of<SettingsProfileBloc>(context);
+            bloc.add(
+              OnChangeSettingsStepEvent(
+                step: SettingsStep.settings,
+              ),
+            );
+          },
+        ),
+        SizedBox(height: $constants.insets.md),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+          child: _buildOptionsList(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOptionsList(BuildContext context) {
+    final bloc = BlocProvider.of<SettingsProfileBloc>(context);
+    return BlocBuilder<SettingsProfileBloc, SettingsProfileState>(
+      builder: (context, state) {
+        return Column(
+          children: PushNotificationOptions.values.map((option) {
+            return Padding(
+              padding: EdgeInsets.only(top: $constants.insets.sm),
+              child: SenpaiSwitchWithTitle(
+                value: bloc.selectedPushNotificationOptions.contains(option),
+                title: option.optionsString,
+                onChanged: (isOn) {
+                  bloc.add(
+                    OnChangePushNotificationOptionsEvent(
+                      option: option,
+                      isOn: isOn,
+                    ),
+                  );
+                },
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
