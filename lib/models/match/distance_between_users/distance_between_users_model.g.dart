19e804a456cc6a1da5decc7766323ec34b9112ab
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-28 18:42:49 +0200

    preview profile add distance between users model

---
+++ lib/models/match/distance_between_users/distance_between_users_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'distance_between_users_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_DistanceBetweenUsersModel _$$_DistanceBetweenUsersModelFromJson(
+        Map<String, dynamic> json) =>
+    _$_DistanceBetweenUsersModel(
+      mi: json['mi'] as int,
+    );
+
+Map<String, dynamic> _$$_DistanceBetweenUsersModelToJson(
+        _$_DistanceBetweenUsersModel instance) =>
+    <String, dynamic>{
+      'mi': instance.mi,
+    };
