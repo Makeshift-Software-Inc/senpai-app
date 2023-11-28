0f35b2f5ce18a4b7c263cd98707446ca12c8e520
vika_litslink <viktoriia.lekh@litslink.com>
2023-11-28 18:49:06 +0200

    preview profile add match circle button

---
+++ lib/screens/preview_profile/widgets/senpai_match_circle_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:senpai/utils/constants.dart';
+
+class SenpaiMatchCircleButton extends StatelessWidget {
+  final String? icon;
+  final VoidCallback onTap;
+  final Widget? customIcon;
+  final double? customPadding;
+  final bool isReverceColor;
+
+  const SenpaiMatchCircleButton({
+    super.key,
+    this.icon,
+    required this.onTap,
+    this.customIcon,
+    this.customPadding,
+    this.isReverceColor = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: isReverceColor
+            ? $constants.palette.white
+            : $constants.palette.lightBlue,
+        padding: const EdgeInsets.all(0),
+      ),
+      child: Container(
+        width: 64,
+        height: 64,
+        padding: EdgeInsetsDirectional.all(customPadding ?? 20),
+        child: _buildIcon(),
+      ),
+    );
+  }
+
+  Widget _buildIcon() {
+    if (icon != null) {
+      return SvgPicture.asset(
+        icon!,
+        width: $constants.insets.md,
+        height: $constants.insets.md,
+        colorFilter: ColorFilter.mode(
+          isReverceColor
+              ? $constants.palette.lightBlue
+              : $constants.palette.white,
+          BlendMode.srcIn,
+        ),
+      );
+    }
+
+    if (customIcon != null) {
+      return customIcon!;
+    }
+    return const SizedBox();
+  }
+}
