904a34888ca66bb524fc848a40a243782357b9a1
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-05 02:32:06 +0300

    feat: Add channel ID to MatchUserData model for room navigation
    - Introduce string id field in MatchUserData model
    - This id will act as the channel ID for user matches
    - Utilize the new id field to open a chat room upon match selection
    
    The addition of a string id to the MatchUserData model enhances
    the functionality of match handling in the app. This id serves
    as a unique identifier for each match, doubling as the channel
    ID for establishing communication channels. The update enables
    efficient navigation and room opening processes when a user
    selects a match, streamlining the chat functionality and
    improving the overall user experience in the application.

--- lib/models/match/match_user_data.dart
+++ lib/models/match/match_user_data.dart
@@ -1,9 +1,11 @@
 class MatchUserData {
+  final String id;
   final String imageUrl;
   final String userName;
   final bool isOnline;
 
   MatchUserData({
+    required this.id,
     required this.imageUrl,
     required this.userName,
     this.isOnline = false,
