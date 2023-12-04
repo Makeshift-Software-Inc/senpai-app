9db3f211e4d983a10ed57c9df59cc016f6e8a709
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-05 02:32:06 +0300

    feat: Add channel ID to MatchUserData model for room navigation
    - Introduce string id field in MatchUserData model
    - This id will act as the channel ID for user matches
    - Utilize the new id field to open a chat room upon match selection
    
    The addition of a string id to the MatchUserData model enhances
    the functionality of match handling in the app. This id serves
    as a unique identifier for each match, doubling as the channel
    ID for establishing communication channels. The update enables
    efficient navigation and room opening processes when a user
    selects a match, streamlining the chat functionality and
    improving the overall user experience in the application.

--- lib/data/mock_match_data.dart
+++ lib/data/mock_match_data.dart
@@ -2,30 +2,35 @@ import 'package:senpai/models/match/match_user_data.dart';
 
 final List<MatchUserData> mockMatchedUsers = [
   MatchUserData(
+    id: "1",
     imageUrl:
         "https://images.pexels.com/photos/5226193/pexels-photo-5226193.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     isOnline: true,
     userName: "Mike",
   ),
   MatchUserData(
+    id: "2",
     imageUrl:
         "https://images.pexels.com/photos/4926674/pexels-photo-4926674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     isOnline: true,
     userName: "Lewis",
   ),
   MatchUserData(
+    id: "3",
     imageUrl:
         "https://images.pexels.com/photos/13876068/pexels-photo-13876068.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     isOnline: false,
     userName: "Fernando",
   ),
   MatchUserData(
+    id: "4",
     imageUrl:
         "https://images.pexels.com/photos/6426224/pexels-photo-6426224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     isOnline: false,
     userName: "Henry",
   ),
   MatchUserData(
+    id: "5",
     imageUrl:
         "https://images.pexels.com/photos/7256511/pexels-photo-7256511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     isOnline: true,
