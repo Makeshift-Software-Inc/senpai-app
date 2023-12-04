f71187dc1c8be42f65db2bafe6c1eb92f8c834e3
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-05 00:38:58 +0300

    feat(Auth): Use auth token to tell if user is showing up for the first time.

--- lib/routes/guards/onboarding_guard.dart
+++ lib/routes/guards/onboarding_guard.dart
@@ -1,29 +1,23 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fresh_dio/fresh_dio.dart';
+import 'package:senpai/dependency_injection/injection.dart';
+import 'package:senpai/models/auth/auth_model.dart';
 import 'package:senpai/routes/app_router.dart';
 
 class ExistingUserGuard extends AutoRouteGuard {
   Future<bool> isFirstVisit() async {
-    const storage = FlutterSecureStorage();
-    final firstVisit = await storage.read(key: 'first_visit');
-    return firstVisit == null;
-  }
-
-  Future<void> setFirstVisit() async {
-    const storage = FlutterSecureStorage();
-    await storage.write(key: 'first_visit', value: 'visited');
+    final storage = getIt<TokenStorage<AuthModel>>();
+    AuthModel? authModel = await storage.read();
+    return authModel == null;
   }
 
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) async {
     final isNewUser = await isFirstVisit();
 
-    if (isNewUser) {
-      await setFirstVisit();
-      resolver.next(true);
-    } else {
+    if (!isNewUser) {
       await router.push(const EntryRoute());
-      resolver.next(true);
     }
+    resolver.next(true);
   }
 }
