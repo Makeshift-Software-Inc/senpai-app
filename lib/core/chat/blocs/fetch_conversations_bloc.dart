601fa5401676693a0f8a58d0f904f9374fb1e1cc
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-05 02:38:17 +0300

    feat: Enhance parseData in fetch_conversations_bloc for data organization
    - Update the parseData method in FetchConversationsBloc
    - Implement logic to categorize conversation data
    - Differentiate between 'matches' and 'active conversations'
    - Populate MatchUserData and ChatConversation lists accordingly
    - Sort active conversations based on the most recent message time
    - Ensure matches are identified by empty message lists
    
    This update to the parseData method of FetchConversationsBloc
    brings significant enhancements to chat data handling. It
    categorizes conversations into matches and active conversations.
    Matches are identified as conversations with empty message lists,
    and active conversations are sorted by the timestamp of the
    latest message. This categorization aids in a more organized
    and efficient rendering of chat data, improving the overall
    user experience in the application's chat feature.

--- lib/core/chat/blocs/fetch_conversations_bloc.dart
+++ lib/core/chat/blocs/fetch_conversations_bloc.dart
@@ -7,6 +7,9 @@ import 'package:senpai/core/graphql/blocs/query/query_bloc.dart';
 import 'package:senpai/core/graphql/models/graphql_api.graphql.dart';
 import 'package:senpai/dependency_injection/injection.dart';
 import 'package:senpai/models/auth/auth_model.dart';
+import 'package:senpai/models/chat/categorized_conversation.dart';
+import 'package:senpai/models/chat/chat_conversation.dart';
+import 'package:senpai/models/match/match_user_data.dart';
 
 @injectable
 class FetchConversationsBloc extends QueryBloc<FetchConversations$Query> {
@@ -56,6 +59,57 @@ class FetchConversationsBloc extends QueryBloc<FetchConversations$Query> {
 
   @override
   FetchConversations$Query parseData(Map<String, dynamic>? data) {
-    return FetchConversations$Query.fromJson(data ?? <String, dynamic>{});
+    Map<String, dynamic> responseJson =
+        FetchConversations$Query.fromJson(data ?? <String, dynamic>{})
+            as Map<String, dynamic>;
+    var conversations = responseJson['data'] as List<dynamic>;
+
+    // Categorize conversations
+    var matches = <MatchUserData>[];
+    var activeConversations = <ChatConversation>[];
+
+    for (var conversation in conversations) {
+      dynamic user = conversation["match"]["user"];
+      if (conversation.messages.isEmpty) {
+        matches.add(MatchUserData(
+          id: conversation["id"],
+          imageUrl: user["gallery"]["photos"][0]["url"],
+          isOnline: user["isOnline"],
+          userName: user["firstName"],
+        ));
+      } else {
+        List<dynamic> messages = conversation["messages"];
+
+        // sort message such that the last message sent is always at index 0
+        messages.sort((a, b) => DateTime.parse(b["createdAt"])
+            .compareTo(DateTime.parse(a["createdAt"])));
+
+        int unreadMessagesCount = 0;
+
+        for (var message in conversation["messages"]) {
+          if (!message["read"]) {
+            unreadMessagesCount++;
+          }
+        }
+
+        activeConversations.add(ChatConversation(
+          id: conversation["id"],
+          profileUrl: user["gallery"]["photos"][0]["url"],
+          contactName: user["firstName"],
+          lastMessage: messages[0]["content"],
+          lastMessageTime: DateTime.parse(messages[0]["createdAt"]),
+          unreadMessagesCount: unreadMessagesCount,
+        ));
+      }
+    }
+
+    // sort active conversations such that the conversation with the most recent message is always at index 0
+    activeConversations
+        .sort((a, b) => b.lastMessageTime.compareTo(a.lastMessageTime));
+
+    return CategorizedConversations(
+      matches: matches,
+      activeConversations: activeConversations,
+    ) as FetchConversations$Query;
   }
 }
