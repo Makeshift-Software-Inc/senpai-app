2514d32ce3fde587208d87ec7d8a7ccf4a34bd6d
vika_litslink <viktoriia.lekh@litslink.com>
2023-12-14 19:47:59 +0200

    match screen add bloc

---
+++ lib/screens/match/bloc/match_state.dart
@@ -0,0 +1,31 @@
+part of 'match_bloc.dart';
+
+@immutable
+abstract class MatchState {}
+
+class MatchInitial extends MatchState {}
+
+class ErrorState extends MatchState {
+  final String message;
+  final bool isEnabled;
+
+  ErrorState({required this.message, required this.isEnabled});
+}
+
+class ValidUserIdState extends MatchState {}
+
+class ValidState extends MatchState {}
+
+class LoadingState extends MatchState {}
+
+class MatchSucssesfulState extends MatchState {}
+
+class NextUserState extends MatchState {
+  final Swipe swipe;
+  final int selectedUserId;
+  NextUserState({required this.swipe, required this.selectedUserId});
+}
+
+class ValidSwipeState extends MatchState {}
+
+class ValidChangePageState extends MatchState {}
