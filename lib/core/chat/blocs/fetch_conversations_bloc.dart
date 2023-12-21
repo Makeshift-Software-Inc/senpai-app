53328f78e0c7e6295ac3bc521267472b05e59066
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-21 19:15:59 +0300

    refactor(chat list): Move stateless
    - rewrite the ChatListController to be stateless to make for a more readable and consistent codebase.

--- lib/core/chat/blocs/fetch_conversations_bloc.dart
+++ lib/core/chat/blocs/fetch_conversations_bloc.dart
@@ -9,19 +9,44 @@ import 'package:senpai/dependency_injection/injection.dart';
 import 'package:senpai/models/auth/auth_model.dart';
 import 'package:senpai/models/chat/categorized_conversation.dart';
 
+// We are using the long polling technique to fetch new messages
+// from the server. This is a very simple implementation of the
+// long polling technique. We are using 30 seconds as the interval
+// to fetch new messages from the server. This is not the best
+// implementation of the long polling technique. We are using this
+// implementation because it is simple and it works.
+
 class FetchConversationsBloc extends QueryBloc<FetchConversations$Query> {
   final Duration refetchInterval;
 
   late AuthModel _authModel;
 
   FetchConversationsBloc({this.refetchInterval = const Duration(seconds: 30)})
-      : super(options: _fetchConversationsQueryOptions());
+      : super(options: _fetchConversationsQueryOptions()) {
+    _initialize();
+  }
 
   static WatchQueryOptions<Object?> _fetchConversationsQueryOptions() {
     return WatchQueryOptions(
       document: FETCH_CONVERSATIONS_QUERY_DOCUMENT,
       fetchPolicy: FetchPolicy.noCache,
     );
+  }
+
+  void _initialize() {
+    _fetchAuthModel();
+    startPeriodicFetch();
+  }
+
+  Future<void> _fetchAuthModel() async {
+    final storage = getIt<TokenStorage<AuthModel>>();
+    AuthModel? authModel = await storage.read();
+    if (authModel == null) {
+      throw Exception("Could not find a signed in user");
+    }
+    _authModel = authModel;
+    final variables = FetchUserArguments(userId: authModel.user.id).toJson();
+    run(variables: variables);
   }
 
   final _conversationsParser = ConversationsParser();
