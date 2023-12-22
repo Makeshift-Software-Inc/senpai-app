da419ffabf108075f649a912b3c6008b1d6f3ab0
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-22 19:36:29 +0300

    fix(routing): Fix onboarding guard
    - Update guard to correctly get firstTime app users
    - Update navigation to clear stack on entry and home routes navigation.

--- lib/routes/guards/onboarding_guard.dart
+++ lib/routes/guards/onboarding_guard.dart
@@ -1,23 +1,31 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:fresh_dio/fresh_dio.dart';
-import 'package:senpai/dependency_injection/injection.dart';
-import 'package:senpai/models/auth/auth_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:senpai/routes/app_router.dart';
 
 class ExistingUserGuard extends AutoRouteGuard {
-  Future<bool> isFirstVisit() async {
-    final storage = getIt<TokenStorage<AuthModel>>();
-    AuthModel? authModel = await storage.read();
-    return authModel == null;
+  // A method to check if the app has been opened before.
+  Future<bool> hasOpenedAppBefore() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('hasOpenedAppBefore') ?? false;
+  }
+
+  // A method to mark the app as opened.
+  Future<void> markAppAsOpened() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('hasOpenedAppBefore', true);
   }
 
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) async {
-    final isNewUser = await isFirstVisit();
+    final hasOpenedBefore = await hasOpenedAppBefore();
 
-    if (!isNewUser) {
-      await router.push(const EntryRoute());
+    if (!hasOpenedBefore) {
+      // If the app hasn't been opened before, mark it as opened and proceed.
+      await markAppAsOpened();
+      resolver.next(true);
+    } else {
+      // If the app has been opened before, redirect to the EntryRoute.
+      await router.replaceAll([const EntryRoute()]);
     }
-    resolver.next(true);
   }
 }
