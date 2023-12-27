9cf653490c5f2ccf0e59b20fd2489cf0088c86e0
vika_litslink <viktoriia.lekh@litslink.com>
2023-12-27 12:29:10 +0200

    add spotify mapper

---
+++ lib/models/user_profile/mappers/user_spotify_mapper.dart
@@ -0,0 +1,47 @@
+import 'package:senpai/core/graphql/models/graphql_api.graphql.dart';
+import 'package:senpai/models/profile_fill/spotify/spotify_artist/spotify_artist_model.dart';
+import 'package:senpai/models/profile_fill/spotify/spotify_track/spotify_track_model.dart';
+import 'package:senpai/models/user_profile/user_favorite_music/user_favorite_music_model.dart';
+import 'package:senpai/screens/profile_fill/spotify/enums/spotify_enums.dart';
+
+extension UserFavoriteArtistMapper on SpotifyArtistModel {
+  UserFavoriteMusicModel toUserFavoriteArtistModel() {
+    return UserFavoriteMusicModel(
+      id: id,
+      artistName: name,
+      coverUrl: images?.first.url ?? '',
+      musicType: MusicType.artist.name,
+      spotifyId: id,
+    );
+  }
+}
+
+extension UserFavoriteTrackMapper on SpotifyTrackModel {
+  UserFavoriteMusicModel toUserFavoriteTrackModel() {
+    final List<String> artistNames =
+        album.artists.map((artist) => artist.name).toList();
+    final artistName = artistNames.join(", ");
+
+    return UserFavoriteMusicModel(
+      id: id,
+      artistName: artistName,
+      trackName: name,
+      coverUrl: album.images?.first.url ?? '',
+      musicType: MusicType.track.name,
+      spotifyId: id,
+    );
+  }
+}
+
+extension UserModelMapper on UserFavoriteMusicModel {
+  FavoriteMusicInput toUserFavoriteMusicInput(int userId) {
+    return FavoriteMusicInput(
+      userId: userId,
+      artistName: artistName,
+      trackName: trackName,
+      coverUrl: coverUrl,
+      musicType: musicType,
+      spotifyId: spotifyId ?? '',
+    );
+  }
+}
