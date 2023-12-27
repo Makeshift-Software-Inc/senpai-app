41c3ed49a9112a8c44259ab409e5c5972d90500e
vika_litslink <viktoriia.lekh@litslink.com>
2023-12-27 13:23:58 +0200

    add disconnect spotify widget

---
+++ lib/screens/edit_profile/edit_spotify/widgets/disconnect_spotify_widget.dart
@@ -0,0 +1,40 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/screens/edit_profile/edit_spotify/bloc/edit_spotify_bloc.dart';
+
+import 'package:senpai/utils/methods/utils.dart';
+
+class DisconnectSpotifyWidget extends StatelessWidget {
+  const DisconnectSpotifyWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<EditSpotifyBloc, EditSpotifyState>(
+      listenWhen: (_, currState) =>
+          currState is SpotifyDisconnectSucssesfulState,
+      listener: (context, state) async {
+        await context.router.pop();
+      },
+      builder: (context, state) {
+        final bloc = BlocProvider.of<EditSpotifyBloc>(context);
+        if (bloc.isShowDisconnectSpotify) {
+          return TextButton(
+            onPressed: () {
+              bloc.add(OnSpotifyDisconnectEvent());
+            },
+            child: Text(
+              TextConstants.disconnectSpotifyText,
+              style: getTextTheme(context).titleSmall,
+            ),
+          );
+        }
+        return const SizedBox();
+      },
+    );
+  }
+}
