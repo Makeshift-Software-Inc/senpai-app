aaee403d311a10b76658a1b63e3969e8943c6626
vika_litslink <viktoriia.lekh@litslink.com>
2023-12-27 13:19:01 +0200

    add spotify fetch music repository

---
+++ lib/domain/profile_fill/spotify/spotify_fetch_user_info_repository.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:senpai/core/profile_fill/api/spotify/spotify_profile_info_api.dart';
+import 'package:senpai/domain/profile_fill/spotify/spotify_failure.dart';
+
+import 'package:senpai/domain/profile_fill/spotify/spotify_fetch_user_info_usecase.dart';
+import 'package:senpai/models/profile_fill/spotify/spotify_artist/spotify_artist_model.dart';
+import 'package:senpai/models/profile_fill/spotify/spotify_track/spotify_track_model.dart';
+
+class SpotifyFetchUserInfoRepository implements SpotifyFetchUserInfoUseCase {
+  final SpotifyFetchUserInfoApi api;
+
+  SpotifyFetchUserInfoRepository(this.api);
+
+  @override
+  Future<Either<SpotifyFailure, List<SpotifyArtistModel>>>
+      getTopArtists() async {
+    try {
+      final artists = await api.getTopArtists();
+      return right(artists);
+    } catch (e) {
+      return left(
+        const ServerError(),
+      );
+    }
+  }
+
+  @override
+  Future<Either<SpotifyFailure, List<SpotifyTrackModel>>> getTopTracks() async {
+    try {
+      final tracks = await api.getTopTracks();
+      return right(tracks);
+    } catch (e) {
+      return left(
+        const ServerError(),
+      );
+    }
+  }
+}
