2cf74d1542fad702a539a9c378ef760a6d974374
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-12-31 00:50:34 +0300

    feat(pagination): Add a paginated model

--- lib/i18n/en/strings.g.dart
+++ lib/i18n/en/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 1
 /// Strings: 7
 ///
-/// Built on 2023-12-22 at 01:30 UTC
+/// Built on 2023-12-30 at 21:48 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
---
+++ lib/models/paginated_model.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'paginated_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PaginatedModel<T> _$PaginatedModelFromJson<T>(
+  Map<String, dynamic> json,
+  T Function(Object? json) fromJsonT,
+) =>
+    PaginatedModel<T>(
+      currentPage: json['currentPage'] as int,
+      items: (json['items'] as List<dynamic>).map(fromJsonT).toList(),
+    );
+
+Map<String, dynamic> _$PaginatedModelToJson<T>(
+  PaginatedModel<T> instance,
+  Object? Function(T value) toJsonT,
+) =>
+    <String, dynamic>{
+      'currentPage': instance.currentPage,
+      'items': instance.items.map(toJsonT).toList(),
+    };
