1fa85a0136bb422c83dca66a516607fed1e2e47e
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-01-17 01:27:51 +0300

    feat(chat): Integrate conversation subscription bloc

--- lib/screens/chat_list/page/chat_list_controller.dart
+++ lib/screens/chat_list/page/chat_list_controller.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/core/action_cable/blocs/action_cable_bloc.dart';
 import 'package:senpai/core/chat/blocs/fetch_conversations_bloc.dart';
+import 'package:senpai/core/chat/blocs/room_subscriptions/conversation_subscription_bloc.dart';
 import 'package:senpai/core/graphql/blocs/query/query_bloc.dart';
 import 'package:senpai/core/widgets/loading.dart';
 import 'package:senpai/data/text_constants.dart';
 import 'package:senpai/screens/chat_list/widgets/chat_list_content.dart';
 import 'package:senpai/utils/helpers/snack_bar_helpers.dart';
+import 'package:senpai/utils/methods/aliases.dart';
 
 class ChatListController extends StatelessWidget {
   const ChatListController({super.key});
@@ -20,25 +23,56 @@ class ChatListController extends StatelessWidget {
     return const SizedBox.shrink();
   }
 
+  void _handleConversationsSubscriptions(
+      BuildContext context, ActionCableState state) {
+    final FetchConversationsBloc fetchConversationsBloc =
+        BlocProvider.of<FetchConversationsBloc>(context);
+    state.maybeWhen(
+      orElse: () {},
+      connected: () {
+        logIt.info("Conversation subscription connected");
+      },
+      data: (data) {
+        logIt.info("Conversation subscription data: $data");
+        fetchConversationsBloc.refetch();
+      },
+      error: (message) {
+        showSnackBarError(context, message);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => FetchConversationsBloc(),
-      child: BlocConsumer<FetchConversationsBloc, QueryState>(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ConversationSubscriptionsBloc>(
+          create: ((context) => ConversationSubscriptionsBloc()),
+        ),
+        BlocProvider<FetchConversationsBloc>(
+          create: (context) => FetchConversationsBloc(),
+        ),
+      ],
+      child: BlocListener<ConversationSubscriptionsBloc, ActionCableState>(
         listener: (context, state) {
-          state.maybeWhen(
-            error: (error, result) {
-              showSnackBarError(context, TextConstants.serverError);
-            },
-            orElse: () {},
-          );
+          _handleConversationsSubscriptions(context, state);
         },
-        builder: (context, state) {
-          return state.maybeWhen(
-            loading: (result) => const SenpaiLoading(),
-            orElse: () => _buildChatListContent(context),
-          );
-        },
+        child: BlocConsumer<FetchConversationsBloc, QueryState>(
+          listener: (context, state) {
+            state.maybeWhen(
+              error: (error, result) {
+                showSnackBarError(context, TextConstants.serverError);
+              },
+              orElse: () {},
+            );
+          },
+          builder: (context, state) {
+            return state.maybeWhen(
+              loading: (result) => const SenpaiLoading(),
+              orElse: () => _buildChatListContent(context),
+            );
+          },
+        ),
       ),
     );
   }
