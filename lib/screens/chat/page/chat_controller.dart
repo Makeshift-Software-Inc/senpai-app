59e3ac5e873e6522e32dfdd7b8a0dc01b2cd912f
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-01-26 03:36:35 +0300

    Move providers above scope to allow attaching widget lifecycle listeners

--- lib/screens/chat/page/chat_controller.dart
+++ lib/screens/chat/page/chat_controller.dart
@@ -3,23 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:senpai/core/action_cable/blocs/action_cable_bloc.dart';
 import 'package:senpai/core/chat/blocs/fetch_messages_bloc.dart';
-import 'package:senpai/core/chat/blocs/fetch_stickers_bloc.dart';
 import 'package:senpai/core/chat/blocs/room_subscriptions/room_subscription_bloc.dart';
-import 'package:senpai/core/chat/blocs/send_message_bloc.dart';
 import 'package:senpai/core/chat/blocs/update_message_bloc.dart';
 import 'package:senpai/core/graphql/blocs/mutation/mutation_bloc.dart';
 import 'package:senpai/core/graphql/blocs/query/query_bloc.dart';
-import 'package:senpai/core/profile_fill/favorite_anime/fetch_anime_bloc.dart';
-import 'package:senpai/core/user/blocs/fetch_user/fetch_user_bloc.dart';
-import 'package:senpai/core/widgets/bottom_sheet/bottom_sheet_bloc.dart';
 import 'package:senpai/core/widgets/loading.dart';
 import 'package:senpai/data/text_constants.dart';
-import 'package:senpai/dependency_injection/injection.dart';
 import 'package:senpai/models/chat/chat_room_params.dart';
 import 'package:senpai/routes/app_router.dart';
-import 'package:senpai/screens/chat/bloc/message_reaction_bloc/message_reaction_bloc.dart';
-import 'package:senpai/screens/chat/bloc/pending_messages_bloc/pending_messages_bloc.dart';
-import 'package:senpai/screens/chat/bloc/text_editing_bloc/text_editing_bloc.dart';
 import 'package:senpai/screens/chat/widgets/chat_content.dart';
 import 'package:senpai/utils/helpers/snack_bar_helpers.dart';
 import 'package:senpai/utils/methods/aliases.dart';
@@ -31,56 +22,29 @@ class ChatController extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final SendMessageBloc sendMessageBloc = SendMessageBloc();
-    final FetchMessagesBloc fetchMessagesBloc =
-        FetchMessagesBloc(roomArgs.roomId);
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<FetchMessagesBloc>(
-          create: (_) => fetchMessagesBloc,
-        ),
-        BlocProvider<SendMessageBloc>(create: (_) => sendMessageBloc),
-        BlocProvider<TextEditingBloc>(create: (_) => TextEditingBloc()),
-        BlocProvider<PendingMessagesBloc>(
-            create: (_) => PendingMessagesBloc(
-                sendMessageBloc: sendMessageBloc,
-                roomArgs: roomArgs,
-                fetchMessagesBloc: fetchMessagesBloc)),
-        BlocProvider<MessageReactionBloc>(create: (_) => MessageReactionBloc()),
-        BlocProvider<BottomSheetBloc>(create: (_) => BottomSheetBloc()),
-        BlocProvider<RoomSubscriptionsBloc>(
-            create: (_) => RoomSubscriptionsBloc(roomId: roomArgs.roomId)),
-        BlocProvider<UpdateMessageBloc>(create: (_) => UpdateMessageBloc()),
-        BlocProvider<FetchStickersBloc>(create: (_) => FetchStickersBloc()),
-        BlocProvider<FetchUserBloc>(
-            create: (_) => FetchUserBloc()
-              ..fetchUser(userId: int.parse(roomArgs.currentUser.id))),
-        BlocProvider<FetchAnimeBloc>(create: (_) => getIt<FetchAnimeBloc>()),
-      ],
-      child: BlocListener<UpdateMessageBloc, MutationState>(
+    return BlocListener<UpdateMessageBloc, MutationState>(
+      listener: (context, state) {
+        _handleUpdateMessageState(context, state);
+      },
+      child: BlocListener<RoomSubscriptionsBloc, ActionCableState>(
         listener: (context, state) {
-          _handleUpdateMessageState(context, state);
+          _handleRoomSubscriptions(context, state);
         },
-        child: BlocListener<RoomSubscriptionsBloc, ActionCableState>(
+        child: BlocListener<FetchMessagesBloc, QueryState>(
           listener: (context, state) {
-            _handleRoomSubscriptions(context, state);
+            _buildFetchMessagesListeners(context, state);
           },
-          child: BlocListener<FetchMessagesBloc, QueryState>(
-            listener: (context, state) {
-              _buildFetchMessagesListeners(context, state);
-            },
-            child: Scaffold(
-              body: SafeArea(
-                child: Stack(
-                  children: [
-                    ChatContent(
-                      receipientUser: roomArgs.reciepient,
-                      currentUser: roomArgs.currentUser,
-                      roomCreationDate: roomArgs.createdDate,
-                      roomId: roomArgs.roomId,
-                    ),
-                  ],
-                ),
+          child: Scaffold(
+            body: SafeArea(
+              child: Stack(
+                children: [
+                  ChatContent(
+                    receipientUser: roomArgs.reciepient,
+                    currentUser: roomArgs.currentUser,
+                    roomCreationDate: roomArgs.createdDate,
+                    roomId: roomArgs.roomId,
+                  ),
+                ],
               ),
             ),
           ),
