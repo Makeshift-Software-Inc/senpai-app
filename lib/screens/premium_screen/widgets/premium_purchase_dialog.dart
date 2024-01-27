3758e196388c9cbd1489d3654df6655761d1d414
vika_litslink <viktoriia.lekh@litslink.com>
2024-01-27 18:19:03 +0200

    change PurchaseDialog with PurchaseBloc

--- lib/screens/premium_screen/widgets/premium_purchase_dialog.dart
+++ lib/screens/premium_screen/widgets/premium_purchase_dialog.dart
@@ -2,156 +2,209 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:senpai/core/widgets/loading.dart';
+import 'package:senpai/core/widgets/primary_button.dart';
+import 'package:senpai/data/path_constants.dart';
+import 'package:senpai/data/text_constants.dart';
+import 'package:senpai/screens/premium_screen/bloc/purchase_bloc.dart';
 import 'package:senpai/screens/premium_screen/widgets/premium_item_button.dart';
 
-import '../../../core/widgets/primary_button.dart';
-import '../../../data/path_constants.dart';
-import '../../../data/text_constants.dart';
-import '../../../utils/constants.dart';
-import '../../../utils/methods/utils.dart';
-import '../bloc/purchase_bloc.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
 
 void openPremiumPurchaseDialog(BuildContext context) {
   showDialog<String>(
     context: context,
     builder: (_) {
-      return const Dialog(
+      return Dialog(
         backgroundColor: Colors.transparent,
         surfaceTintColor: Colors.transparent,
         insetPadding: EdgeInsets.zero,
-        child: PurchaseDialog(),
+        child: BlocProvider(
+          create: (_) => PurchaseBloc()..add(OnPlanInitEvent()),
+          child: const PurchaseDialog(),
+        ),
       );
     },
   );
 }
 
-class PurchaseDialog extends StatefulWidget {
+class PurchaseDialog extends StatelessWidget {
   const PurchaseDialog({Key? key}) : super(key: key);
 
-  @override
-  State<PurchaseDialog> createState() => _PurchaseDialogState();
-}
-
-class _PurchaseDialogState extends State<PurchaseDialog> {
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => PurchaseBloc(),
-      child: BlocListener<PurchaseBloc, PurchaseState>(
-        listenWhen: (_, currState) => currState is UpdatePlanState,
-        listener: (context, state) {},
-        child:
-            BlocBuilder<PurchaseBloc, PurchaseState>(builder: (context, state) {
-          final bloc = BlocProvider.of<PurchaseBloc>(context);
-
-          if (bloc.selectedSubscription == null) {
-            bloc.add(OnUpdatePlanEvent(
-                subscriptionPlan: bloc.subscriptionPlanList[0]));
-            return const SizedBox.shrink();
-          }
-
-          return Container(
-            margin: EdgeInsets.symmetric(
-              vertical: $constants.corners.sm,
-              horizontal: $constants.insets.sm,
-            ),
-            padding: EdgeInsets.symmetric(
-              vertical: $constants.insets.md,
-              horizontal: $constants.insets.sm,
-            ),
-            decoration: BoxDecoration(
-              color: $constants.palette.appBackground,
-              borderRadius: BorderRadius.circular($constants.insets.sm),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildCrownIcon(
-                  width: $constants.insets.lg,
-                  height: $constants.insets.lg,
-                ),
-                SizedBox(height: $constants.insets.xs),
-                AutoSizeText(
-                  TextConstants.outOfSuperLikes,
-                  style: getTextTheme(context).headlineLarge?.copyWith(),
-                  maxLines: 1,
-                ),
-                SizedBox(height: $constants.insets.xs),
-                Text(
-                  TextConstants.doNotLoseDiamonds,
-                  style: getTextTheme(context).headlineSmall?.copyWith(
-                        fontWeight: FontWeight.w500,
-                      ),
-                ),
-                SizedBox(height: $constants.insets.md),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    for (var i = 0; i < bloc.subscriptionPlanList.length; i++)
-                      PremiumItemButton(
-                        onPressed: () {
-                          bloc.add(OnUpdatePlanEvent(
-                            subscriptionPlan: bloc.subscriptionPlanList[i],
-                          ));
-                        },
-                        subscriptionPlan: bloc.subscriptionPlanList[i],
-                        selectedSubscription: bloc.selectedSubscription,
-                      ),
-                  ],
-                ),
-                SizedBox(height: $constants.insets.xl),
-                _buildBuyPremiumButton(
-                    context, () {}, bloc.selectedSubscription!.price),
-                SizedBox(height: $constants.insets.xs),
-                _noThanksButton(context),
-              ],
-            ),
-          );
-        }),
+  void _onTapBuy(BuildContext context) {
+    final bloc = BlocProvider.of<PurchaseBloc>(context);
+    final Map<String, PurchaseDetails> purchases =
+        Map<String, PurchaseDetails>.fromEntries(
+      bloc.purchases.map((PurchaseDetails purchase) {
+        if (purchase.pendingCompletePurchase) {
+          bloc.inAppPurchase.completePurchase(purchase);
+        }
+        return MapEntry<String, PurchaseDetails>(
+          purchase.productID,
+          purchase,
+        );
+      }),
+    );
+    bloc.add(
+      OnTapBuyConsumableEvent(
+        productDetails: bloc.selectedProductDetails!,
+        purchases: purchases,
       ),
     );
   }
-}
 
-Widget _buildCrownIcon({required double width, required double height}) {
-  return SvgPicture.asset(
-    PathConstants.crownIcon,
-    width: width,
-    height: height,
-    fit: BoxFit.contain,
-    colorFilter: ColorFilter.mode(
-      $constants.palette.yellow,
-      BlendMode.srcIn,
-    ),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<PurchaseBloc, PurchaseState>(
+      listener: (context, state) {
+        final bloc = BlocProvider.of<PurchaseBloc>(context);
 
-Widget _buildBuyPremiumButton(
-    BuildContext context, Function() onPressed, String price) {
-  return Padding(
-    padding: EdgeInsets.symmetric(
-      horizontal: $constants.insets.md,
-      vertical: $constants.insets.sm,
-    ),
-    child: PrimaryButton(
-      text: '${TextConstants.dialogBuyButton} $price',
-      onPressed: onPressed,
-      backgroundColor: $constants.palette.gold,
-    ),
-  );
-}
+        if (bloc.isPurchased == true) {
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        final bloc = BlocProvider.of<PurchaseBloc>(context);
+        Widget content = const SizedBox();
+        if (state is PurchaseErrorState && state.message.isNotEmpty) {
+          content = _buildErrorDialog(state.message);
+        } else if (bloc.isAvailablePurchase == false) {
+          content = _buildErrorDialog(
+            TextConstants.unableConnectPaymentsProcessor,
+          );
+        } else if (bloc.isAvailablePurchase != null) {
+          content = _buildContent(context);
+        }
+        return Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: $constants.corners.sm,
+                horizontal: $constants.insets.sm,
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: $constants.insets.md,
+                horizontal: $constants.insets.sm,
+              ),
+              decoration: BoxDecoration(
+                color: $constants.palette.appBackground,
+                borderRadius: BorderRadius.circular($constants.insets.sm),
+              ),
+              child: content,
+            ),
+            if (state is LoadingState) const SenpaiLoading(),
+          ],
+        );
+      },
+    );
+  }
 
-Widget _noThanksButton(BuildContext context) {
-  return TextButton(
-    onPressed: () {
-      Navigator.pop(context);
-    },
-    child: Text(
-      TextConstants.noThanks.toUpperCase(),
-      style: getTextTheme(context).bodySmall,
-    ),
-  );
+  Widget _buildContent(BuildContext context) {
+    final bloc = BlocProvider.of<PurchaseBloc>(context);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildCrownIcon(
+          width: $constants.insets.lg,
+          height: $constants.insets.lg,
+        ),
+        SizedBox(height: $constants.insets.xs),
+        AutoSizeText(
+          TextConstants.outOfSuperLikes,
+          style: getTextTheme(context).headlineLarge?.copyWith(),
+          maxLines: 1,
+        ),
+        SizedBox(height: $constants.insets.xs),
+        Text(
+          TextConstants.doNotLoseDiamonds,
+          style: getTextTheme(context).headlineSmall?.copyWith(
+                fontWeight: FontWeight.w500,
+              ),
+        ),
+        SizedBox(height: $constants.insets.md),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: bloc.subscriptionPlanList
+              .map((plan) => PremiumItemButton(
+                    onPressed: () {
+                      bloc.add(OnUpdatePlanEvent(subscriptionPlan: plan));
+                    },
+                    subscriptionPlan: plan,
+                    selectedSubscription: bloc.selectedSubscription,
+                  ))
+              .toList(),
+        ),
+        SizedBox(height: $constants.insets.xl),
+        if (bloc.selectedSubscription != null)
+          _buildBuyPremiumButton(
+            context,
+            () {
+              _onTapBuy(context);
+            },
+            bloc.selectedSubscription?.price ?? '',
+          ),
+        SizedBox(height: $constants.insets.xs),
+        _noThanksButton(context),
+      ],
+    );
+  }
+
+  Widget _buildCrownIcon({required double width, required double height}) {
+    return SvgPicture.asset(
+      PathConstants.crownIcon,
+      width: width,
+      height: height,
+      fit: BoxFit.contain,
+      colorFilter: ColorFilter.mode(
+        $constants.palette.yellow,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+
+  Widget _buildBuyPremiumButton(
+      BuildContext context, Function() onPressed, String price) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: $constants.insets.md,
+        vertical: $constants.insets.sm,
+      ),
+      child: PrimaryButton(
+        text: '${TextConstants.dialogBuyButton} $price',
+        onPressed: onPressed,
+        backgroundColor: $constants.palette.gold,
+      ),
+    );
+  }
+
+  Widget _noThanksButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: Text(
+        TextConstants.noThanks.toUpperCase(),
+        style: getTextTheme(context).bodySmall,
+      ),
+    );
+  }
+
+  Widget _buildErrorDialog(String text) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.warning, color: $constants.palette.red),
+        SizedBox(height: $constants.insets.xs),
+        Text(text)
+      ],
+    );
+  }
 }
