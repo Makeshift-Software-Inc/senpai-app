fc222efce5fec1a627be396b41527812d2fc695e
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-02-02 03:38:46 +0300

    feat(notifications): Listen to incoming notifications

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:senpai/core/app/app.dart';
+import 'package:senpai/core/notification/notification_manager.dart';
 // import 'package:senpai/core/bloc_observer/bloc_observer.dart';
 import 'package:senpai/core/sentry/sentry_module.dart';
 import 'package:senpai/dependency_injection/injection.dart';
@@ -34,7 +35,7 @@ Future<void> main() async {
 
     final messaging = FirebaseMessaging.instance;
 
-    final settings = await messaging.requestPermission(
+    await messaging.requestPermission(
       alert: true,
       announcement: false,
       badge: true,
@@ -44,16 +45,14 @@ Future<void> main() async {
       sound: true,
     );
 
-    if (env.debug) {
-      logIt.debug('User granted permission: ${settings.authorizationStatus}');
-    }
-
     String? token = await messaging.getToken();
     if (env.debug) {
       logIt.debug('Token: $token');
     }
-    // TODO: Set up foreground message handler
-    // TODO: Set up background message handler
+
+    // Listen to notifications.
+    final notificationManager = NotificationManager();
+    notificationManager.listenToNotifications();
 
     // Set bloc observer and hydrated bloc storage.
     // Bloc.observer = Observer();
