72b7d93b0e5e75c43cb18f19933846d6ecdc27c2
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-02-09 02:17:09 +0300

    Update auto generated files

--- lib/core/graphql/models/graphql_api.graphql.dart
+++ lib/core/graphql/models/graphql_api.graphql.dart
@@ -237,6 +237,80 @@ class AddDeviceTokenInput extends JsonSerializable with EquatableMixin {
 }
 
 @JsonSerializable(explicitToJson: true)
+class RemoveDeviceToken$Mutation$RemoveDeviceToken$User extends JsonSerializable
+    with EquatableMixin {
+  RemoveDeviceToken$Mutation$RemoveDeviceToken$User();
+
+  factory RemoveDeviceToken$Mutation$RemoveDeviceToken$User.fromJson(
+          Map<String, dynamic> json) =>
+      _$RemoveDeviceToken$Mutation$RemoveDeviceToken$UserFromJson(json);
+
+  late String id;
+
+  @override
+  List<Object?> get props => [id];
+  @override
+  Map<String, dynamic> toJson() =>
+      _$RemoveDeviceToken$Mutation$RemoveDeviceToken$UserToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class RemoveDeviceToken$Mutation$RemoveDeviceToken extends JsonSerializable
+    with EquatableMixin {
+  RemoveDeviceToken$Mutation$RemoveDeviceToken();
+
+  factory RemoveDeviceToken$Mutation$RemoveDeviceToken.fromJson(
+          Map<String, dynamic> json) =>
+      _$RemoveDeviceToken$Mutation$RemoveDeviceTokenFromJson(json);
+
+  late RemoveDeviceToken$Mutation$RemoveDeviceToken$User user;
+
+  @override
+  List<Object?> get props => [user];
+  @override
+  Map<String, dynamic> toJson() =>
+      _$RemoveDeviceToken$Mutation$RemoveDeviceTokenToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class RemoveDeviceToken$Mutation extends JsonSerializable with EquatableMixin {
+  RemoveDeviceToken$Mutation();
+
+  factory RemoveDeviceToken$Mutation.fromJson(Map<String, dynamic> json) =>
+      _$RemoveDeviceToken$MutationFromJson(json);
+
+  RemoveDeviceToken$Mutation$RemoveDeviceToken? removeDeviceToken;
+
+  @override
+  List<Object?> get props => [removeDeviceToken];
+  @override
+  Map<String, dynamic> toJson() => _$RemoveDeviceToken$MutationToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class RemoveDeviceTokenInput extends JsonSerializable with EquatableMixin {
+  RemoveDeviceTokenInput({
+    this.clientMutationId,
+    required this.deviceToken,
+    required this.userId,
+  });
+
+  factory RemoveDeviceTokenInput.fromJson(Map<String, dynamic> json) =>
+      _$RemoveDeviceTokenInputFromJson(json);
+
+  String? clientMutationId;
+
+  late String deviceToken;
+
+  late String userId;
+
+  @override
+  List<Object?> get props => [clientMutationId, deviceToken, userId];
+  @override
+  Map<String, dynamic> toJson() => _$RemoveDeviceTokenInputToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
 class SubmitVerifyRequest$Mutation$SubmitVerifyRequest$User
     extends JsonSerializable with EquatableMixin {
   SubmitVerifyRequest$Mutation$SubmitVerifyRequest$User();
@@ -3632,80 +3706,6 @@ class FetchStickers$Query extends JsonSerializable with EquatableMixin {
 }
 
 @JsonSerializable(explicitToJson: true)
-class RemoveDeviceToken$Mutation$RemoveDeviceToken$User extends JsonSerializable
-    with EquatableMixin {
-  RemoveDeviceToken$Mutation$RemoveDeviceToken$User();
-
-  factory RemoveDeviceToken$Mutation$RemoveDeviceToken$User.fromJson(
-          Map<String, dynamic> json) =>
-      _$RemoveDeviceToken$Mutation$RemoveDeviceToken$UserFromJson(json);
-
-  late String id;
-
-  @override
-  List<Object?> get props => [id];
-  @override
-  Map<String, dynamic> toJson() =>
-      _$RemoveDeviceToken$Mutation$RemoveDeviceToken$UserToJson(this);
-}
-
-@JsonSerializable(explicitToJson: true)
-class RemoveDeviceToken$Mutation$RemoveDeviceToken extends JsonSerializable
-    with EquatableMixin {
-  RemoveDeviceToken$Mutation$RemoveDeviceToken();
-
-  factory RemoveDeviceToken$Mutation$RemoveDeviceToken.fromJson(
-          Map<String, dynamic> json) =>
-      _$RemoveDeviceToken$Mutation$RemoveDeviceTokenFromJson(json);
-
-  late RemoveDeviceToken$Mutation$RemoveDeviceToken$User user;
-
-  @override
-  List<Object?> get props => [user];
-  @override
-  Map<String, dynamic> toJson() =>
-      _$RemoveDeviceToken$Mutation$RemoveDeviceTokenToJson(this);
-}
-
-@JsonSerializable(explicitToJson: true)
-class RemoveDeviceToken$Mutation extends JsonSerializable with EquatableMixin {
-  RemoveDeviceToken$Mutation();
-
-  factory RemoveDeviceToken$Mutation.fromJson(Map<String, dynamic> json) =>
-      _$RemoveDeviceToken$MutationFromJson(json);
-
-  RemoveDeviceToken$Mutation$RemoveDeviceToken? removeDeviceToken;
-
-  @override
-  List<Object?> get props => [removeDeviceToken];
-  @override
-  Map<String, dynamic> toJson() => _$RemoveDeviceToken$MutationToJson(this);
-}
-
-@JsonSerializable(explicitToJson: true)
-class RemoveDeviceTokenInput extends JsonSerializable with EquatableMixin {
-  RemoveDeviceTokenInput({
-    this.clientMutationId,
-    required this.deviceToken,
-    required this.userId,
-  });
-
-  factory RemoveDeviceTokenInput.fromJson(Map<String, dynamic> json) =>
-      _$RemoveDeviceTokenInputFromJson(json);
-
-  String? clientMutationId;
-
-  late String deviceToken;
-
-  late String userId;
-
-  @override
-  List<Object?> get props => [clientMutationId, deviceToken, userId];
-  @override
-  Map<String, dynamic> toJson() => _$RemoveDeviceTokenInputToJson(this);
-}
-
-@JsonSerializable(explicitToJson: true)
 class DeleteFavoriteAnimeArguments extends JsonSerializable
     with EquatableMixin {
   DeleteFavoriteAnimeArguments({required this.input});
@@ -3978,6 +3978,94 @@ class AddDeviceTokenMutation
   @override
   AddDeviceToken$Mutation parse(Map<String, dynamic> json) =>
       AddDeviceToken$Mutation.fromJson(json);
+}
+
+@JsonSerializable(explicitToJson: true)
+class RemoveDeviceTokenArguments extends JsonSerializable with EquatableMixin {
+  RemoveDeviceTokenArguments({required this.input});
+
+  @override
+  factory RemoveDeviceTokenArguments.fromJson(Map<String, dynamic> json) =>
+      _$RemoveDeviceTokenArgumentsFromJson(json);
+
+  late RemoveDeviceTokenInput input;
+
+  @override
+  List<Object?> get props => [input];
+  @override
+  Map<String, dynamic> toJson() => _$RemoveDeviceTokenArgumentsToJson(this);
+}
+
+final REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT_OPERATION_NAME =
+    'removeDeviceToken';
+final REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT = DocumentNode(definitions: [
+  OperationDefinitionNode(
+    type: OperationType.mutation,
+    name: NameNode(value: 'removeDeviceToken'),
+    variableDefinitions: [
+      VariableDefinitionNode(
+        variable: VariableNode(name: NameNode(value: 'input')),
+        type: NamedTypeNode(
+          name: NameNode(value: 'RemoveDeviceTokenInput'),
+          isNonNull: true,
+        ),
+        defaultValue: DefaultValueNode(value: null),
+        directives: [],
+      )
+    ],
+    directives: [],
+    selectionSet: SelectionSetNode(selections: [
+      FieldNode(
+        name: NameNode(value: 'removeDeviceToken'),
+        alias: null,
+        arguments: [
+          ArgumentNode(
+            name: NameNode(value: 'input'),
+            value: VariableNode(name: NameNode(value: 'input')),
+          )
+        ],
+        directives: [],
+        selectionSet: SelectionSetNode(selections: [
+          FieldNode(
+            name: NameNode(value: 'user'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: SelectionSetNode(selections: [
+              FieldNode(
+                name: NameNode(value: 'id'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null,
+              )
+            ]),
+          )
+        ]),
+      )
+    ]),
+  )
+]);
+
+class RemoveDeviceTokenMutation extends GraphQLQuery<RemoveDeviceToken$Mutation,
+    RemoveDeviceTokenArguments> {
+  RemoveDeviceTokenMutation({required this.variables});
+
+  @override
+  final DocumentNode document = REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT;
+
+  @override
+  final String operationName =
+      REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT_OPERATION_NAME;
+
+  @override
+  final RemoveDeviceTokenArguments variables;
+
+  @override
+  List<Object?> get props => [document, operationName, variables];
+  @override
+  RemoveDeviceToken$Mutation parse(Map<String, dynamic> json) =>
+      RemoveDeviceToken$Mutation.fromJson(json);
 }
 
 @JsonSerializable(explicitToJson: true)
@@ -8157,92 +8245,4 @@ class FetchStickersQuery
   @override
   FetchStickers$Query parse(Map<String, dynamic> json) =>
       FetchStickers$Query.fromJson(json);
-}
-
-@JsonSerializable(explicitToJson: true)
-class RemoveDeviceTokenArguments extends JsonSerializable with EquatableMixin {
-  RemoveDeviceTokenArguments({required this.input});
-
-  @override
-  factory RemoveDeviceTokenArguments.fromJson(Map<String, dynamic> json) =>
-      _$RemoveDeviceTokenArgumentsFromJson(json);
-
-  late RemoveDeviceTokenInput input;
-
-  @override
-  List<Object?> get props => [input];
-  @override
-  Map<String, dynamic> toJson() => _$RemoveDeviceTokenArgumentsToJson(this);
-}
-
-final REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT_OPERATION_NAME =
-    'removeDeviceToken';
-final REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT = DocumentNode(definitions: [
-  OperationDefinitionNode(
-    type: OperationType.mutation,
-    name: NameNode(value: 'removeDeviceToken'),
-    variableDefinitions: [
-      VariableDefinitionNode(
-        variable: VariableNode(name: NameNode(value: 'input')),
-        type: NamedTypeNode(
-          name: NameNode(value: 'RemoveDeviceTokenInput'),
-          isNonNull: true,
-        ),
-        defaultValue: DefaultValueNode(value: null),
-        directives: [],
-      )
-    ],
-    directives: [],
-    selectionSet: SelectionSetNode(selections: [
-      FieldNode(
-        name: NameNode(value: 'removeDeviceToken'),
-        alias: null,
-        arguments: [
-          ArgumentNode(
-            name: NameNode(value: 'input'),
-            value: VariableNode(name: NameNode(value: 'input')),
-          )
-        ],
-        directives: [],
-        selectionSet: SelectionSetNode(selections: [
-          FieldNode(
-            name: NameNode(value: 'user'),
-            alias: null,
-            arguments: [],
-            directives: [],
-            selectionSet: SelectionSetNode(selections: [
-              FieldNode(
-                name: NameNode(value: 'id'),
-                alias: null,
-                arguments: [],
-                directives: [],
-                selectionSet: null,
-              )
-            ]),
-          )
-        ]),
-      )
-    ]),
-  )
-]);
-
-class RemoveDeviceTokenMutation extends GraphQLQuery<RemoveDeviceToken$Mutation,
-    RemoveDeviceTokenArguments> {
-  RemoveDeviceTokenMutation({required this.variables});
-
-  @override
-  final DocumentNode document = REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT;
-
-  @override
-  final String operationName =
-      REMOVE_DEVICE_TOKEN_MUTATION_DOCUMENT_OPERATION_NAME;
-
-  @override
-  final RemoveDeviceTokenArguments variables;
-
-  @override
-  List<Object?> get props => [document, operationName, variables];
-  @override
-  RemoveDeviceToken$Mutation parse(Map<String, dynamic> json) =>
-      RemoveDeviceToken$Mutation.fromJson(json);
 }
--- lib/utils/r.dart
+++ lib/utils/r.dart
@@ -2,6 +2,7 @@
 
 // ignore_for_file: avoid_classes_with_only_static_members,always_specify_types,lines_longer_than_80_chars,non_constant_identifier_names,prefer_double_quotes,unnecessary_raw_strings,use_raw_strings
 
+
 class R {
   static const images = _ImageResources();
   static const svg = _SvgResources();
@@ -10,77 +11,79 @@ class R {
 class _ImageResources {
   const _ImageResources();
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/launcher.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/launcher.png)
   final launcher = r'assets/images/launcher.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/logo.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/logo.png)
   final logo = r'assets/images/logo.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/onboarding/onboarding_1.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/onboarding/onboarding_1.png)
   final onboarding_1 = r'assets/images/onboarding/onboarding_1.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/onboarding/onboarding_3.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/onboarding/onboarding_3.png)
   final onboarding_3 = r'assets/images/onboarding/onboarding_3.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/onboarding/onboarding_2.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/onboarding/onboarding_2.png)
   final onboarding_2 = r'assets/images/onboarding/onboarding_2.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/onboarding/onboarding_bg.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/onboarding/onboarding_bg.png)
   final onboarding_bg = r'assets/images/onboarding/onboarding_bg.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/onboarding/heart.svg)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/onboarding/heart.svg)
   final heart = r'assets/images/onboarding/heart.svg';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/start_match_bg.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/start_match_bg.png)
   final start_match_bg = r'assets/images/profile/start_match_bg.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/photo_not_verified.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/photo_not_verified.png)
   final photo_not_verified = r'assets/images/profile/photo_not_verified.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/photo_verifed.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/photo_verifed.png)
   final photo_verifed = r'assets/images/profile/photo_verifed.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/location_bg.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/location_bg.png)
   final location_bg = r'assets/images/profile/location_bg.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/welcome_image.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/welcome_image.png)
   final welcome_image = r'assets/images/profile/welcome_image.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/verify_user_photo.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/verify_user_photo.png)
   final verify_user_photo = r'assets/images/profile/verify_user_photo.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/premium_image.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/premium_image.png)
   final premium_image = r'assets/images/profile/premium_image.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/lines_bg.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/lines_bg.png)
   final lines_bg = r'assets/images/profile/lines_bg.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/marker_icon.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/marker_icon.png)
   final marker_icon = r'assets/images/profile/marker_icon.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/anime_empty.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/anime_empty.png)
   final anime_empty = r'assets/images/profile/anime_empty.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/anime_header.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/anime_header.png)
   final anime_header = r'assets/images/profile/anime_header.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/profile/spotify_bg.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/profile/spotify_bg.png)
   final spotify_bg = r'assets/images/profile/spotify_bg.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/match/super_like_bg.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/match/super_like_bg.png)
   final super_like_bg = r'assets/images/match/super_like_bg.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/match/match_successful.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/match/match_successful.png)
   final match_successful = r'assets/images/match/match_successful.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/chat/empty_conversation_search.png)
-  final empty_conversation_search =
-      r'assets/images/chat/empty_conversation_search.png';
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/chat/empty_conversation_search.png)
+  final empty_conversation_search = r'assets/images/chat/empty_conversation_search.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/chat/empty_animations.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/chat/empty_animations.png)
   final empty_animations = r'assets/images/chat/empty_animations.png';
 
-  /// ![](/Users/alexeypodolianpro/Projects/work/senpai-app/assets/images/chat/empty_conversations.png)
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/chat/giphy_attribution.png)
+  final giphy_attribution = r'assets/images/chat/giphy_attribution.png';
+
+  /// ![](/Users/daniel/Desktop/work/senpai/assets/images/chat/empty_conversations.png)
   final empty_conversations = r'assets/images/chat/empty_conversations.png';
 }
 
