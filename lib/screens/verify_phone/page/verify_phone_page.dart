faac2d42c17c42975811ce53c9ac9f534ba65bd1
vika_litslink <viktoriia.lekh@litslink.com>
2024-02-25 18:35:42 +0200

    add change bloc builder on bloc listener in verify photo page

--- lib/screens/verify_phone/page/verify_phone_page.dart
+++ lib/screens/verify_phone/page/verify_phone_page.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fresh_dio/fresh_dio.dart';
@@ -16,6 +15,7 @@ import 'package:senpai/models/auth/user_model.dart';
 import 'package:senpai/routes/app_router.dart';
 import 'package:senpai/screens/verify_phone/blocs/otp_form_bloc/otp_form_bloc.dart';
 import 'package:senpai/screens/verify_phone/widget/verify_phone_content.dart';
+import 'package:senpai/utils/helpers/snack_bar_helpers.dart';
 import 'package:senpai/utils/methods/aliases.dart';
 
 @RoutePage()
@@ -45,190 +45,176 @@ class VerifyPhonePage extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: [
-            BlocConsumer<OTPFormBloc, OtpFormState>(
-              listener: (context, state) {
-                if (state.isSubmitting) {
-                  final serviceBloc =
-                      BlocProvider.of<ValidatePhoneBloc>(context);
-                  final formBloc = BlocProvider.of<OTPFormBloc>(context);
-
-                  final code = formBloc.state.otpCode;
-
-                  serviceBloc.validateUserAccount(int.parse(code), id);
-                }
-              },
-              builder: (context, state) {
-                return VerifyPhoneContent(
-                  phone: phone,
-                  id: id,
-                );
-              },
-            ),
-            BlocBuilder<ValidatePhoneBloc, MutationState>(
-              builder: (context, state) {
-                return state.maybeWhen<Widget>(
-                    loading: () => const SenpaiLoading(),
-                    failed: (error, result) {
-                      final formBloc = BlocProvider.of<OTPFormBloc>(context);
-                      formBloc.add(const OtpFormEvent.failed());
-                      WidgetsBinding.instance.addPostFrameCallback(
-                        (_) {
-                          ScaffoldMessenger.of(context)
-                            ..hideCurrentSnackBar()
-                            ..showSnackBar(
-                              SnackBar(
-                                /// need to set following properties for best effect of awesome_snackbar_content
-                                elevation: 0,
-                                behavior: SnackBarBehavior.floating,
-                                backgroundColor: Colors.transparent,
-                                content: AwesomeSnackbarContent(
-                                  title: 'On Snap!',
-                                  message: R.strings.serverError,
-
-                                  /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
-                                  contentType: ContentType.failure,
-                                ),
-                              ),
-                            );
-                        },
-                      );
-
-                      return const SizedBox.shrink();
-                    },
-                    succeeded: (data, result) {
-                      final response = result.data;
-
-                      if (response == null) {
-                        // handle this fatal error
-                        logIt.wtf(
-                            "A successful empty response just got recorded");
-                        return const SizedBox.shrink();
-                      }
-
-                      String token = response["validatePhone"]["token"];
-                      bool hasFilledProfile =
-                          response["validatePhone"]["profileFilled"];
-                      UserModel user =
-                          UserModel.fromJson(response["validatePhone"]["user"]);
-                      final formBloc = BlocProvider.of<OTPFormBloc>(context);
-                      formBloc.isProfileFilled = hasFilledProfile;
-
-                      final storage = getIt<TokenStorage<AuthModel>>();
-                      storage.write(AuthModel(token: token, user: user));
-
-                      // save the device token for notifications
-                      final addDeviceTokenBloc = getIt<AddDeviceTokenBloc>();
-                      addDeviceTokenBloc.checkStorageAndAddDeviceToken(
-                        userId: user.id,
-                      );
-
-                      final serviceBloc = BlocProvider.of<SignInBloc>(context);
-                      serviceBloc.signInExistingUser(token);
-                      return const SenpaiLoading();
-                    },
-                    orElse: () => const SizedBox.shrink());
-              },
-            ),
-            BlocBuilder<ResendVerificationCodeBloc, MutationState>(
-              builder: (context, state) {
-                return state.maybeWhen<Widget>(
-                    loading: () => const SenpaiLoading(),
-                    failed: (error, result) {
-                      final formBloc = BlocProvider.of<OTPFormBloc>(context);
-                      formBloc.add(const OtpFormEvent.failed());
-                      WidgetsBinding.instance.addPostFrameCallback(
-                        (_) {
-                          ScaffoldMessenger.of(context)
-                            ..hideCurrentSnackBar()
-                            ..showSnackBar(
-                              SnackBar(
-                                /// need to set following properties for best effect of awesome_snackbar_content
-                                elevation: 0,
-                                behavior: SnackBarBehavior.floating,
-                                backgroundColor: Colors.transparent,
-                                content: AwesomeSnackbarContent(
-                                  title: 'On Snap!',
-                                  message: R.strings.serverError,
-
-                                  /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
-                                  contentType: ContentType.failure,
-                                ),
-                              ),
-                            );
-                        },
-                      );
-
-                      return const SizedBox.shrink();
-                    },
-                    orElse: () => const SizedBox.shrink());
-              },
-            ),
-            BlocBuilder<SignInBloc, MutationState>(
-              builder: (context, state) {
-                return state.maybeWhen<Widget>(
-                    loading: () => const SenpaiLoading(),
-                    failed: (error, result) {
-                      final formBloc = BlocProvider.of<OTPFormBloc>(context);
-                      formBloc.add(const OtpFormEvent.failed());
-                      WidgetsBinding.instance.addPostFrameCallback(
-                        (_) {
-                          ScaffoldMessenger.of(context)
-                            ..hideCurrentSnackBar()
-                            ..showSnackBar(
-                              SnackBar(
-                                /// need to set following properties for best effect of awesome_snackbar_content
-                                elevation: 0,
-                                behavior: SnackBarBehavior.floating,
-                                backgroundColor: Colors.transparent,
-                                content: AwesomeSnackbarContent(
-                                  title: 'On Snap!',
-                                  message: R.strings.serverError,
-
-                                  /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
-                                  contentType: ContentType.failure,
-                                ),
-                              ),
-                            );
-                        },
-                      );
-
-                      return const SizedBox.shrink();
-                    },
-                    succeeded: (data, result) {
-                      final response = result.data;
-
-                      if (response == null) {
-                        // handle this fatal error
-                        logIt.wtf(
-                            "A successful empty response just got recorded");
-                        return const SizedBox.shrink();
-                      }
-
-                      String id = response["signIn"]["user"]["id"];
-                      String phone = response["signIn"]["user"]["phone"];
-                      print("signed in user of id $id and phone $phone");
-
-                      final formBloc = BlocProvider.of<OTPFormBloc>(context);
-                      bool hasFilledProfile = formBloc.isProfileFilled;
-
-                      if (hasFilledProfile) {
-                        WidgetsBinding.instance.addPostFrameCallback((_) {
-                          context.router.replaceAll([HomeRoute()]);
-                        });
-                      } else {
-                        context.router.push(ProfileFillRoute(
-                          id: id,
-                          phone: phone,
-                        ));
-                      }
-
-                      return const SizedBox.shrink();
-                    },
-                    orElse: () => const SizedBox.shrink());
-              },
-            )
+            _buildOTPFormListeners(),
+            _buildValidatePhoneBlocListeners(),
+            _buildResendVerificationCodeListeners(),
+            _buildSignInListeners(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildOTPFormListeners() {
+    return BlocConsumer<OTPFormBloc, OtpFormState>(
+      listener: (context, state) {
+        if (state.isSubmitting) {
+          final serviceBloc = BlocProvider.of<ValidatePhoneBloc>(context);
+          final formBloc = BlocProvider.of<OTPFormBloc>(context);
+
+          final code = formBloc.state.otpCode;
+
+          serviceBloc.validateUserAccount(int.parse(code), id);
+        }
+      },
+      builder: (context, state) {
+        return VerifyPhoneContent(
+          phone: phone,
+          id: id,
+        );
+      },
+    );
+  }
+
+  Widget _buildValidatePhoneBlocListeners() {
+    return BlocListener<ValidatePhoneBloc, MutationState>(
+      listener: (context, state) {
+        state.whenOrNull(
+          failed: (error, result) {
+            final formBloc = BlocProvider.of<OTPFormBloc>(context);
+            formBloc.add(const OtpFormEvent.failed());
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              showSnackBarError(context, R.strings.serverError);
+            });
+          },
+          succeeded: (data, result) {
+            final response = result.data;
+            if (response == null) {
+              logIt.wtf("A successful empty response just got recorded");
+              return;
+            }
+            dynamic model;
+            try {
+              model = response["validatePhone"];
+              String token = model["token"];
+              bool hasFilledProfile = model["profileFilled"];
+              UserModel user = UserModel.fromJson(model["user"]);
+              final formBloc = BlocProvider.of<OTPFormBloc>(context);
+              formBloc.isProfileFilled = hasFilledProfile;
+
+              final storage = getIt<TokenStorage<AuthModel>>();
+              storage.write(AuthModel(token: token, user: user));
+
+              // save the device token for notifications
+              final addDeviceTokenBloc = getIt<AddDeviceTokenBloc>();
+              addDeviceTokenBloc.checkStorageAndAddDeviceToken(
+                userId: user.id,
+              );
+
+              final serviceBloc = BlocProvider.of<SignInBloc>(context);
+              serviceBloc.signInExistingUser(token);
+            } catch (e) {
+              logIt.error("Error accessing validatePhone from response: $e");
+              model = null;
+            }
+            if (model == null) {
+              showSnackBarError(context, R.strings.serverError);
+              logIt.error("A validatePhone with error");
+            }
+          },
+        );
+      },
+      child: BlocBuilder<ValidatePhoneBloc, MutationState>(
+        builder: (context, state) {
+          return state.maybeWhen<Widget>(
+            loading: () => const SenpaiLoading(),
+            orElse: () => const SizedBox.shrink(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildResendVerificationCodeListeners() {
+    return BlocListener<ResendVerificationCodeBloc, MutationState>(
+      listener: (context, state) {
+        state.whenOrNull(
+          failed: (error, result) {
+            final formBloc = BlocProvider.of<OTPFormBloc>(context);
+            formBloc.add(const OtpFormEvent.failed());
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              showSnackBarError(context, R.strings.serverError);
+            });
+          },
+        );
+      },
+      child: BlocBuilder<ResendVerificationCodeBloc, MutationState>(
+        builder: (context, state) {
+          return state.maybeWhen<Widget>(
+            loading: () => const SenpaiLoading(),
+            orElse: () => const SizedBox.shrink(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildSignInListeners() {
+    return BlocListener<SignInBloc, MutationState>(
+      listener: (context, state) {
+        state.whenOrNull(
+          failed: (error, result) {
+            final formBloc = BlocProvider.of<OTPFormBloc>(context);
+            formBloc.add(const OtpFormEvent.failed());
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              showSnackBarError(context, R.strings.serverError);
+            });
+          },
+          succeeded: (data, result) {
+            final response = result.data;
+            if (response == null) {
+              logIt.wtf("A successful empty response just got recorded");
+              return;
+            }
+            dynamic model;
+            try {
+              model = response["signIn"]["user"];
+              String id = model["id"];
+              String phone = model["phone"];
+
+              print("signed in user of id $id and phone $phone");
+
+              final formBloc = BlocProvider.of<OTPFormBloc>(context);
+              bool hasFilledProfile = formBloc.isProfileFilled;
+
+              if (hasFilledProfile) {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  context.router.replaceAll([HomeRoute()]);
+                });
+              } else {
+                context.router.push(ProfileFillRoute(
+                  id: id,
+                  phone: phone,
+                ));
+              }
+            } catch (e) {
+              logIt.error("Error accessing signIn or user from response: $e");
+              model = null;
+            }
+            if (model == null) {
+              showSnackBarError(context, R.strings.serverError);
+              logIt.error("A signIn with error");
+            }
+          },
+        );
+      },
+      child: BlocBuilder<SignInBloc, MutationState>(
+        builder: (context, state) {
+          return state.maybeWhen<Widget>(
+            loading: () => const SenpaiLoading(),
+            orElse: () => const SizedBox.shrink(),
+          );
+        },
       ),
     );
   }
