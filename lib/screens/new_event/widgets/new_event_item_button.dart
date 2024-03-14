01c9aa65cabea7f02c3d4fcfac7adc83df9f87af
vika_litslink <viktoriia.lekh@litslink.com>
2024-03-14 22:29:38 +0200

    add NewEventItemButton

---
+++ lib/screens/new_event/widgets/new_event_item_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:senpai/utils/constants.dart';
+import 'package:senpai/utils/methods/utils.dart';
+
+class NewEventItemButton extends StatelessWidget {
+  final String title;
+  final String description;
+
+  final VoidCallback? onTap;
+  final Widget customWidget;
+
+  const NewEventItemButton({
+    super.key,
+    required this.title,
+    this.description = '',
+    this.onTap,
+    required this.customWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      behavior: HitTestBehavior.opaque,
+      child: Container(
+        padding: EdgeInsets.all($constants.insets.sm),
+        decoration: profileBoxDecoration(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: getTextTheme(context).bodyMedium?.copyWith(
+                  color: $constants.palette.white, fontWeight: FontWeight.w600),
+            ),
+            if (description.isNotEmpty)
+              Text(
+                ' ($description)',
+                style: getTextTheme(context).bodyMedium?.copyWith(
+                      fontSize: 14,
+                      color: $constants.palette.white,
+                    ),
+              ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: customWidget,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
