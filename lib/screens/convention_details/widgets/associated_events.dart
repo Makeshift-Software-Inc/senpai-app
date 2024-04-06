8a1bd6ce2a2f0ca360cb91fefcbc78378c96a2db
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-04-07 01:33:39 +0300

    feat: Add filters to associated events

--- lib/screens/convention_details/widgets/associated_events.dart
+++ lib/screens/convention_details/widgets/associated_events.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:senpai/l10n/resources.dart';
+import 'package:senpai/screens/convention_details/bloc/associated_events_bloc/associated_events_bloc.dart';
 import 'package:senpai/utils/constants.dart';
 import 'package:senpai/utils/methods/utils.dart';
 
@@ -8,22 +10,83 @@ class AssociatedEvents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: $constants.palette.white,
-        borderRadius: BorderRadius.circular($constants.corners.xlg),
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: $constants.corners.xxl,
-            child: Text(
-              R.strings.associatedEventsTitle,
-              style: getTextTheme(context).headlineSmall,
-            ),
-          )
-        ],
+    return BlocBuilder<AssociatedEventsFilterCubit,
+        AssociatedEventsFilterState>(
+      builder: (context, state) {
+        return Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: $constants.palette.white,
+            borderRadius: BorderRadius.circular($constants.corners.xlg),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: $constants.corners.xxl,
+                child: Text(
+                  R.strings.associatedEventsTitle,
+                  style: getTextTheme(context).headlineSmall,
+                ),
+              ),
+              SizedBox(height: $constants.insets.sm),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildFilterButton(context,
+                      title: R.strings.openInvitesFilterTitle,
+                      isActive: state == AssociatedEventsFilterState.open,
+                      onTap: () {
+                    context
+                        .read<AssociatedEventsFilterCubit>()
+                        .setFilterToOpen();
+                  }),
+                  SizedBox(width: $constants.insets.sm),
+                  _buildFilterButton(context,
+                      title: R.strings.closedInvitesFilterTitle,
+                      isActive: state == AssociatedEventsFilterState.filled,
+                      onTap: () {
+                    context
+                        .read<AssociatedEventsFilterCubit>()
+                        .setFilterToFilled();
+                  }),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterButton(
+    BuildContext context, {
+    required bool isActive,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: $constants.insets.sm,
+          vertical: $constants.insets.xs,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular($constants.corners.xlg),
+          color: isActive ? $constants.palette.white : $constants.palette.black,
+          border: Border.all(
+            color: $constants.palette.buttonBorder,
+          ),
+        ),
+        child: Text(
+          title,
+          style: getTextTheme(context).headlineSmall!.copyWith(
+                color: isActive
+                    ? $constants.palette.black
+                    : $constants.palette.white,
+                fontSize: 14,
+              ),
+        ),
       ),
     );
   }
