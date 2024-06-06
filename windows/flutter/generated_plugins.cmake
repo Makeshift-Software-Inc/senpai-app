9c6494f2c1d32e17bd77e779154dc4998c434f39
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-06-07 00:08:37 +0300

    Add flutter unity integration package

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_core
   flutter_secure_storage_windows
+  flutter_unity_widget
   geolocator_windows
   permission_handler_windows
   sentry_flutter
