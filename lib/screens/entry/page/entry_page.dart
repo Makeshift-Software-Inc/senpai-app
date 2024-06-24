6c4a199c129f7e1b9d648afe250110d09971f10f
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-06-25 00:17:48 +0300

    debug: temporarily send sing in to lobby

--- lib/screens/entry/page/entry_page.dart
+++ lib/screens/entry/page/entry_page.dart
@@ -63,8 +63,8 @@ class EntryPage extends StatelessWidget {
                 signInBloc.signInUser(context.router, result.data);
             if (isSignedIn) {
               WidgetsBinding.instance.addPostFrameCallback((_) {
-                context.router.replaceAll([HomeRoute()]);
-                // context.router.replaceAll([UnityViewRoute()]);
+                // context.router.replaceAll([HomeRoute()]);
+                context.router.replaceAll([LobbyRoute()]);
               });
             }
             return const SizedBox.shrink();
