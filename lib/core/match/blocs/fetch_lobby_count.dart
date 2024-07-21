5874507067d23440b45064aef34801477615d821
Ssejjemba Dan <ssejjembadan@gmail.com>
2024-07-21 23:48:33 +0300

    Add BLOC for fetch lobby content

---
+++ lib/core/match/blocs/fetch_lobby_count.dart
@@ -0,0 +1,29 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:senpai/core/graphql/blocs/query/query_bloc.dart';
+import 'package:senpai/core/graphql/models/graphql_api.graphql.dart';
+
+class FetchLobbyCount extends QueryBloc<FetchLobbyCount$Query> {
+  FetchLobbyCount() : super(options: _fetchLobbyCountQueryOptions());
+
+  static WatchQueryOptions<Object?> _fetchLobbyCountQueryOptions() {
+    return WatchQueryOptions(
+      document: FETCH_LOBBY_COUNT_QUERY_DOCUMENT,
+      fetchPolicy: FetchPolicy.noCache,
+    );
+  }
+
+  int lobbyCount = 0;
+
+  Future<void> fetchLobbyCount(int userId) async {
+    final variables = FetchLobbyCountArguments(userId: userId).toJson();
+    run(variables: variables);
+  }
+
+  @override
+  FetchLobbyCount$Query parseData(Map<String, dynamic>? data) {
+    final parsedData =
+        FetchLobbyCount$Query.fromJson(data ?? <String, dynamic>{});
+    lobbyCount = parsedData.fetchLobbyCount;
+    return parsedData;
+  }
+}
