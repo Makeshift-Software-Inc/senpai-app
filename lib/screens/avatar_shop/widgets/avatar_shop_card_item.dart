39d666363c5744d17365ab287175a8b1e4bb1b5d
vika_litslink <viktoriia.lekh@litslink.com>
2024-08-20 18:42:49 +0300

    change avatar shop page and content

--- lib/screens/avatar_shop/widgets/avatar_shop_card_item.dart
+++ lib/screens/avatar_shop/widgets/avatar_shop_card_item.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:senpai/core/avatar_shop/blocs/grant_user_avatar_bloc.dart';
 import 'package:senpai/core/widgets/gradient_filled_button.dart';
 import 'package:senpai/core/widgets/gradient_outlined_button.dart';
 import 'package:senpai/data/path_constants.dart';
+import 'package:senpai/models/avatar_shop/avatar_shop_model.dart';
+import 'package:senpai/screens/avatar_shop/bloc/avatar_shop_bloc.dart';
 import 'package:senpai/utils/constants.dart';
 import 'package:senpai/utils/methods/utils.dart';
 
-import 'avatar_shop_unlock_premium_dialog.dart';
-
 class AvatarShopCardItem extends StatelessWidget {
-  const AvatarShopCardItem({super.key, required this.data});
+  const AvatarShopCardItem({
+    super.key,
+    required this.avatarsShopModel,
+  });
 
-  final Map<String, dynamic> data;
+  final AvatarsShopModel avatarsShopModel;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +26,7 @@ class AvatarShopCardItem extends StatelessWidget {
         child: Stack(
           children: [
             Image.network(
-              data["image"],
+              avatarsShopModel.photoUrl,
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
@@ -52,13 +57,15 @@ class AvatarShopCardItem extends StatelessWidget {
                       ),
                     ),
                     child: CircleAvatar(
-                      backgroundImage: NetworkImage(data["profile"]),
+                      backgroundImage: NetworkImage(
+                        avatarsShopModel.thumbnailUrl,
+                      ),
                       radius: getWidthSize(context, 0.176),
                     ),
                   ),
                   SizedBox(height: getWidthSize(context, 0.015)),
                   Text(
-                    data["name"],
+                    avatarsShopModel.name,
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: getWidthSize(context, 0.032),
@@ -66,55 +73,101 @@ class AvatarShopCardItem extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: getWidthSize(context, 0.02)),
-                  data["status"] == "Emote"
-                      ? GradientOutlinedButton(
-                          title: "Emote",
+                  avatarsShopModel.productId != null &&
+                          avatarsShopModel.productId!.isNotEmpty
+                      ? GradientFilledButton(
+                          //TODO: add to localization
+                          title: "Buy",
+                          startColor: $constants.palette.buyButtonStart,
+                          endColor: $constants.palette.buyButtonEnd,
+                          height: getWidthSize(context, 0.08) + 2,
+                          borderRadius: getWidthSize(context, 0.026),
+                          onPressed: () {
+                            print('Buy');
+                          },
+                        )
+                      : GradientOutlinedButton(
+                          //TODO: add to localization
+                          title: "Download",
                           height: getWidthSize(context, 0.08),
                           borderRadius: getWidthSize(context, 0.026),
                           startColor: $constants.palette.emoteButtonStart,
                           endColor: $constants.palette.emoteButtonEnd,
-                          onPressed: () {})
-                      : data["status"] == "Buy"
-                          ? GradientFilledButton(
-                              title: data["product_id"] != null
-                                  ? "Buy"
-                                  : "Download",
-                              startColor: $constants.palette.buyButtonStart,
-                              endColor: $constants.palette.buyButtonEnd,
-                              height: getWidthSize(context, 0.08) + 2,
-                              borderRadius: getWidthSize(context, 0.026),
-                              onPressed: () {})
-                          : data["status"] == "Premium"
-                              ? GradientFilledButton(
-                                  title: "Premium",
-                                  height: getWidthSize(context, 0.08) + 2,
-                                  borderRadius: getWidthSize(context, 0.026),
-                                  trailingIconPath: PathConstants.crownIcon,
-                                  onPressed: () {
-                                    showAvatarShopUnlockPremiumDialog(context);
-                                  })
-                              : GradientFilledButton(
-                                  title: data["status"],
-                                  height: getWidthSize(context, 0.08) + 2,
-                                  borderRadius: getWidthSize(context, 0.026),
-                                  onPressed: () {})
+                          onPressed: () {
+                            final bloc =
+                                BlocProvider.of<AvatarsShopBloc>(context);
+                            final serviceBloc =
+                                BlocProvider.of<GrantUserAvatarBloc>(context);
+                            serviceBloc.grantUserAvatar(
+                              userId: bloc.userID,
+                              avatarGuid: avatarsShopModel.guid,
+                            );
+                          },
+                        )
+                  //TODO: delete it after add Premium Button
+
+                  // data["status"] == "Emote"
+                  //     ? GradientOutlinedButton(
+                  //         title: "Emote",
+                  //         height: getWidthSize(context, 0.08),
+                  //         borderRadius: getWidthSize(context, 0.026),
+                  //         startColor: $constants.palette.emoteButtonStart,
+                  //         endColor: $constants.palette.emoteButtonEnd,
+                  //         onPressed: () {},
+                  //       )
+                  //     : data["status"] == "Buy"
+                  //         ? GradientFilledButton(
+                  //             title: data["product_id"] != null
+                  //                 ? "Buy"
+                  //                 : "Download",
+                  //             startColor: $constants.palette.buyButtonStart,
+                  //             endColor: $constants.palette.buyButtonEnd,
+                  //             height: getWidthSize(context, 0.08) + 2,
+                  //             borderRadius: getWidthSize(context, 0.026),
+                  //             onPressed: () {},
+                  //           )
+                  //         : data["status"] == "Premium"
+                  //             ? GradientFilledButton(
+                  //                 title: "Premium",
+                  //                 height: getWidthSize(context, 0.08) + 2,
+                  //                 borderRadius: getWidthSize(context, 0.026),
+                  //                 trailingIconPath: PathConstants.crownIcon,
+                  //                 onPressed: () {
+                  //                   showAvatarShopUnlockPremiumDialog(context);
+                  //                 })
+                  //             : GradientFilledButton(
+                  //                 title: data["status"],
+                  //                 height: getWidthSize(context, 0.08) + 2,
+                  //                 borderRadius: getWidthSize(context, 0.026),
+                  //                 onPressed: () {},
+                  //               )
                 ],
               ),
             ),
-            if (data["isNew"])
-              Positioned(
-                top: 0,
-                right: 0,
-                child: Image.asset(
-                  PathConstants.newTag,
-                  fit: BoxFit.fill,
-                  width: getWidthSize(context, 0.157),
-                  height: getWidthSize(context, 0.0527),
-                ),
-              ),
+            _buildNewWidget(context),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildNewWidget(BuildContext context) {
+    final beforeDate = DateTime.now().copyWith(day: DateTime.now().day - 30);
+    bool isNew = avatarsShopModel.createdAt != null &&
+        avatarsShopModel.createdAt!.isAfter(beforeDate);
+
+    if (isNew) {
+      return Positioned(
+        top: 0,
+        right: 0,
+        child: Image.asset(
+          PathConstants.newTag,
+          fit: BoxFit.fill,
+          width: getWidthSize(context, 0.157),
+          height: getWidthSize(context, 0.0527),
+        ),
+      );
+    }
+    return const SizedBox();
   }
 }
